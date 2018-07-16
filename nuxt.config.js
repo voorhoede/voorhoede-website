eebcae4722ef16f1882240c2ebdefd0d7d34bed7
Peter Goes <petergoes@gmail.com>
2018-07-16 13:12:04 +0200

    chore: add Nuxt init

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  srcDir: 'src/client',
+
+  generate: {
+    dir: 'dist/client'
+  },
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'De Voorhoede',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Website for De Voorhoede' },
+      { 'http-equiv': 'Accept-CH', content: 'DPR, Width, Viewport-Width, Save-Data' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'dns-prefetch', href: 'https://www.datocms-assets.com/' },
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
