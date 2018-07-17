bf2fb214fabd1325a0b4cfe1974126f4f773de68
Peter Goes <petergoes@gmail.com>
2018-07-17 14:01:39 +0200

    chore: add svg support

--- config/storybook/webpack.config.js
+++ config/storybook/webpack.config.js
@@ -3,12 +3,19 @@ const path = require('path')
 const nuxtRootDir = path.resolve(`${__dirname}/../../src/client`)
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  const urlLoader = defaultConfig.module.rules.find((rule) => /file-loader/.test(rule.loader))
+  urlLoader.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/
 
   defaultConfig.resolve.alias = {
     vue$: 'vue/dist/vue.esm.js',
     '~': nuxtRootDir,
     '~~': nuxtRootDir,
   }
+
+  defaultConfig.module.rules.push({
+    test: /\.svg$/,
+    loader: 'vue-svg-loader'
+  })
 
   return defaultConfig
 }
