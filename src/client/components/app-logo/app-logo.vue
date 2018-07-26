07820c432e46ac5f1296dcef8c3e0d5b3f39ccd3
Frank Warnaar <frank@voorhoede.nl>
2018-07-26 17:29:13 +0200

    refactor(app-logo): lines in order requested by linter; trailing comma

--- src/client/components/app-logo/app-logo.vue
+++ src/client/components/app-logo/app-logo.vue
@@ -8,13 +8,13 @@ import Logo from '../../static/images/logo.svg'
 import LogoWithText from '../../static/images/logo-with-text.svg'
 
 export default {
+  components: { Logo, LogoWithText },
   props: {
     showText: {
       type: Boolean,
       default: false,
-    }
+    },
   },
-  components: { Logo, LogoWithText },
 }
 </script>
 
