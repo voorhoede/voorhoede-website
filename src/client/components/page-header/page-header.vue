caa42f50b5ac053fb86efa06c5ea7d6591128b6d
Frank Warnaar <frank@voorhoede.nl>
2018-07-30 15:29:01 +0200

    feat(page-header): align bracket on grid

--- src/client/components/page-header/page-header.vue
+++ src/client/components/page-header/page-header.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="page-header grid">
-    <div class="page-header__block"/>
-    <div class="page-header__block"/>
+  <div class="page-header">
+    <div class="page-header__fill-container grid">
+      <div class="page-header__fill"/>
+    </div>
+    <div v-if="curlyBracket" class="page-header__bracket-container grid">
+      <span class="page-header__bracket">}</span>
+    </div>
   </div>
 </template>
 
@@ -18,39 +22,44 @@ export default {
 </script>
 
 <style>
-.page-header {
-  display: block;
-}
-
-.page-header__block {
-  height: 50vh;
-}
-
-.page-header__block:first-child {
-  background: var(--bg-pastel);
-}
-
-.page-header__block:last-child {
-  background: var(--brand-yellow);
+.page-header__bracket {
+  display: none;
 }
 
 @media screen and (min-width: 720px) {
   .page-header {
-    display: grid;
     height: 100vh;
+    position: relative;
   }
 
-  .page-header__block {
-    height: auto;
+  .page-header__fill-container {
+    height: 100%;
   }
 
-  .page-header__block:first-child {
-    grid-column: page-left;
-  }
-
-  .page-header__block:last-child {
-    height: auto;
+  .page-header__fill {
+    height: 100%;
     grid-column: page-right;
+    background: var(--brand-yellow);
+  }
+
+  .page-header__bracket-container {
+    padding: var(--spacing-larger) 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    text-align: right;
+  }
+
+  .page-header__bracket {
+    display: block;
+    grid-column-end: 32;
+  }
+}
+
+@media screen and (min-width: 1100px) {
+  .page-header__bracket {
+    grid-column-end: 48;
   }
 }
 </style>
