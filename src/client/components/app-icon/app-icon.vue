25a25d8a0232f9699620c0481e7e8bae7a448728
Jesse Eikema <jesse@voorhoede.nl>
2018-07-30 13:36:37 +0200

    feat(app-icon): add app-icon component html

---
+++ src/client/components/app-icon/app-icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <img class="app-icon"
+       :src="`/icons/${name}.svg`"
+       :alt="alt">
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      alt: {
+        type: String,
+        default: '',
+      },
+    },
+  }
+</script>
+
+<style scoped>
+.app-icon {
+  width: 1rem;
+  height: 1rem;
+}
+</style>
