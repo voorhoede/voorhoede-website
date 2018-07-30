1856dc6f9c10747c8854a44e3353a4dec9e7babf
Frank Warnaar <frank@voorhoede.nl>
2018-07-30 16:24:53 +0200

    feat(page-header): position curly bracket

--- src/client/components/page-header/page-header.vue
+++ src/client/components/page-header/page-header.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="page-header">
-    <div class="page-header__fill-container grid">
+    <div class="page-header__fill-grid grid">
       <div class="page-header__fill"/>
     </div>
-    <div v-if="curlyBracket" class="page-header__bracket-container grid">
-      <span class="page-header__bracket">}</span>
+    <div v-if="curlyBracket" class="page-header__bracket-grid grid">
+      <div class="page-header__bracket-column">
+        <!-- TODO: Get curly bracket export from Joshua -->
+        <img class="page-header__bracket" src="/images/temporary-curly-bracket.svg">
+      </div>
     </div>
   </div>
 </template>
@@ -22,44 +25,56 @@ export default {
 </script>
 
 <style>
-.page-header__bracket {
+.page-header {
   display: none;
 }
 
-@media screen and (min-width: 720px) {
-  .page-header {
-    height: 100vh;
-    position: relative;
+@supports (display: grid) {
+  @media screen and (min-width: 720px) {
+    .page-header {
+      display: block;
+      height: 100vh;
+      position: relative;
+    }
+
+    .page-header__fill-grid {
+      height: 100%;
+    }
+
+    .page-header__fill {
+      height: 100%;
+      background: var(--brand-yellow);
+      grid-column: page-right;
+    }
+
+    .page-header__bracket-grid {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+    }
+
+    .page-header__bracket-column {
+      grid-column-end: 32;
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .page-header__bracket {
+      padding-top: calc(var(--spacing-larger) + var(--app-header-height));
+      padding-bottom: var(--spacing-larger);      display: block;
+      height: 100%;
+    }
   }
 
-  .page-header__fill-container {
-    height: 100%;
-  }
+  @media screen and (min-width: 1100px) {
+    .page-header__bracket-column {
+      grid-column-end: 48;
+    }
 
-  .page-header__fill {
-    height: 100%;
-    grid-column: page-right;
-    background: var(--brand-yellow);
-  }
-
-  .page-header__bracket-container {
-    padding: var(--spacing-larger) 0;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    text-align: right;
-  }
-
-  .page-header__bracket {
-    display: block;
-    grid-column-end: 32;
-  }
-}
-
-@media screen and (min-width: 1100px) {
-  .page-header__bracket {
-    grid-column-end: 48;
+    .page-header__bracket {
+      padding-top: calc(var(--spacing-larger) + var(--app-header-height-large));
+    }
   }
 }
 </style>
