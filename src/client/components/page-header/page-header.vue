bfcefcfb5545fdf4a31726040f63d4d1f508acdf
Frank Warnaar <frank@voorhoede.nl>
2018-07-30 16:47:06 +0200

    feat(app-header): expose app-header-height from variables.css

--- src/client/components/page-header/page-header.vue
+++ src/client/components/page-header/page-header.vue
@@ -33,16 +33,12 @@ export default {
   @media screen and (min-width: 720px) {
     .page-header {
       display: block;
-      height: 100vh;
       position: relative;
-    }
-
-    .page-header__fill-grid {
-      height: 100%;
+      z-index: -1; /* Make sure page-header doens't overlap content */
     }
 
     .page-header__fill {
-      height: 100%;
+      height: 100vh;
       background: var(--brand-yellow);
       grid-column: page-right;
     }
@@ -65,18 +61,20 @@ export default {
       padding-top: calc(var(--spacing-larger) + var(--app-header-height));
       padding-bottom: var(--spacing-larger);
       display: block;
-      height: 100%;
+      height: 100vh;
       flex-grow: 1;
     }
   }
 
   @media screen and (min-width: 1100px) {
-    .page-header__bracket-column {
-      grid-column-end: 48;
-    }
-
     .page-header__bracket {
       padding-top: calc(var(--spacing-larger) + var(--app-header-height-large));
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    .page-header__bracket-column {
+      grid-column-end: 48;
     }
   }
 }
