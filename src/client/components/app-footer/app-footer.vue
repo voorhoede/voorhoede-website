80bde55b5fc92713a00a44e09ec672c355a7b9b9
Jesse Eikema <jesse@voorhoede.nl>
2018-08-01 17:29:43 +0200

    feat(app-footer): add component

--- src/client/components/app-footer/app-footer.vue
+++ src/client/components/app-footer/app-footer.vue
@@ -1,58 +1,101 @@
 <template>
   <footer class="footer grid">
-    <app-footer-list class="footer__list-wrapper" />
-    <div class="footer__logo">
-      <h2 class="h1">Make it real</h2>
-      <app-logo />
-      <p class="body">Discuss your next project with us</p>
+    <app-footer-list class="footer__wrapper" />
+    <div class="footer__header">
+      <h2 class="h3 footer__header-title">{{ headerTitle }}</h2>
+      <img class="footer__header-logo" src="/images/logo--blue.svg">
+      <p class="body">{{ headerSubtitle }}</p>
     </div>
-    <app-footer-list class="footer__list-wrapper footer__list-wrapper--right" />
-    <div class="footer footer__row">
-      <span class="footer-businessinfo">KvK: 56017235. BTW: NL851944620B01. IBAN: NL20ABNA0442829159</span>
+    <div class="footer__wrapper footer__wrapper--right">
+      <h5 class="body footer-contact__header">Contact</h5>
+      <div class="body-detail footer-contact__link-list">
+        <nuxt-link target="_blank" :to="`tel:${tel}`">{{ tel }}</nuxt-link>
+        <nuxt-link target="_blank" :to="`mailto:${email}`">{{ email }}</nuxt-link>
+        <nuxt-link
+          target="_blank"
+          :to="`${addressLink}`"
+          class="footer-contact__link-address">
+          <span>{{ address }}</span>
+          <span>{{ postalCode }}</span>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <div class="body-detail footer__bottom-text">
+        <span>KvK: {{ kvk }}</span>
+        <span>BTW: {{ btw }}</span>
+        <span>IBAN: {{ iban }}</span>
+      </div>
       <ul class="footer-icon__list">
         <li class="footer-icon__list-item">
-          facebook
+          <nuxt-link to="https://www.instagram.com/devoorhoede/?hl=nl" target="_blank">
+            <app-icon name="instagram--blue" />
+          </nuxt-link>
         </li>
         <li class="footer-icon__list-item">
-          instagram
+          <nuxt-link to="https://twitter.com/devoorhoede" target="_blank">
+            <app-icon name="twitter--blue" />
+          </nuxt-link>
         </li>
         <li class="footer-icon__list-item">
-          twitter
+          <nuxt-link to="https://www.facebook.com/DeVoorhoede/" target="_blank">
+            <app-icon name="facebook--blue" />
+          </nuxt-link>
         </li>
         <li class="footer-icon__list-item">
-          github
+          <nuxt-link to="https://github.com/voorhoede/" target="_blank">
+            <app-icon name="git-hub--blue" />
+          </nuxt-link>
         </li>
       </ul>
-      <span class="footer-copyright">@ De Voorhoede 2018  Privacy statement</span>
+      <span class="body-detail footer-copyright">{{ copyright }}</span>
     </div>
   </footer>
 </template>
 
 <script>
 import AppFooterList from '../../components/app-footer-list'
-import AppLogo from '../../components/app-logo'
+import AppIcon from '../../components/app-icon'
 
 export default {
   components: {
     AppFooterList,
-    AppLogo,
+    AppIcon,
+  },
+  data: () => {
+    return {
+      headerTitle: 'Make it real',
+      headerSubtitle: 'Discuss your next project with us',
+      tel: '+31 (0)20 2610 954',
+      email: 'post@voorhoede.nl',
+      addressLink: 'https://www.google.nl/maps/place/De+Voorhoede+%7C+Front-end+Development/@52.3477995,4.8485761,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5e21d502d2d59:0xbf570944a96ebf45!8m2!3d52.3477962!4d4.8507648',
+      address: 'Rijnsburgstraat 9 - 11',
+      postalCode: '1059 AT Amsterdam',
+      copyright: '@ De Voorhoede 2018  Privacy statement',
+      kvk: '56017235',
+      btw: 'NL851944620B01',
+      iban: 'NL20ABNA0442829159',
+    }
   },
 }
 </script>
 
-
-<style scoped>
+<style>
 .footer {
-  padding: 10px 0;
+  padding: var(--spacing-large) 0 0 0;
   background: var(--brand-yellow);
 }
 
-.footer__row {
+.footer__bottom {
   display: flex;
   flex-direction: column;
+  align-items: center;
+  margin-bottom: calc(var(--spacing-smaller) * 2);
+  padding-top: calc(var(--spacing-smaller) * 2);
+  border-top: 1px solid var(--black);
 }
 
-.footer__row .footer-businessinfo {
+.footer__bottom .footer__bottom-text {
   display: none;
 }
 
@@ -60,72 +103,155 @@ export default {
   text-align: center;
 }
 
+.footer-contact__header {
+  font-weight: 700;
+  color: var(--html-blue);
+  text-align: center;
+  margin-bottom: var(--spacing-tiny);
+}
+
+.footer-contact__link-list {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  flex-flow: wrap;
+}
+
+.footer-contact__link-list a {
+  text-decoration: none;
+  padding: 0 var(--spacing-smaller) 0 0;
+}
+
 .footer-icon__list {
   display: flex;
   text-align: center;
   justify-content: space-around;
-  margin-bottom: 15px;
+  margin-bottom: var(--spacing-small);
+}
+
+.footer-contact__link-address {
+  display: flex;
+}
+
+.footer-contact__link-address span {
+  padding-right: var(--spacing-tiny);
+}
+
+.footer-icon__list-item {
+  margin: 0 calc(var(--spacing-small) / 2);
+}
+
+.footer-icon__list-item a img.app-icon {
+  width: 1.2em;
+  height: 1.2em;
 }
 
 @supports (display: grid) {
-  .footer__list-wrapper {
+  .footer__wrapper {
     text-align: center;
     grid-row: 2;
-    margin-bottom: 20px;
+    margin-bottom: var(--spacing-large);
   }
 
-  .footer__list-wrapper.footer__list-wrapper--right {
+  .footer__wrapper.footer__wrapper--right {
     grid-row: 3;
     text-align: center;
   }
 
-  .footer__logo {
+  .footer__header {
     grid-row: 1;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
-    margin-bottom: 15px;
-  }
-
-  .footer__logo .h1 {
-    margin-bottom: 15px;
-  }
-
-  .footer__logo .body {
-    color: var(--html-blue);
     margin-bottom: 40px;
   }
 
-  .footer__logo svg {
+  .footer__header .footer__header-title {
+    margin-bottom: calc(var(--spacing-smaller) * 2);
+  }
+
+  .footer__header .body {
+    color: var(--html-blue);
+    font-weight: 700;
+    width: 180px;
+    text-align: center;
+  }
+
+  .footer__header img {
     width: 3.25rem;
     height: 3.25rem;
-    margin-bottom: 15px;
+    margin-bottom:var(--spacing-small);
   }
 
   @media (min-width: 720px) {
-     .footer__logo .body {
-       margin-bottom: 0;
-     }
+    .footer-icon__list-item a img.app-icon {
+      width: 1.4em;
+      height: 1.4em;
+    }
 
-    .footer__row {
-      border-top: 1px solid var(--black);
+    .footer__header .body {
+      margin-bottom: 0;
+      width: auto;
+    }
+
+    .footer-copyright {
+      text-align: right;
+      width: 33%;
+    }
+
+    .footer__bottom {
+      border-top: 3px solid var(--html-blue);
       flex-direction: row;
+      justify-content: space-between;
+      align-items: flex-start;
     }
 
-    .footer__row .footer-businessinfo {
+    .footer__bottom .footer__bottom-text {
       display: flex;
+      flex-flow: row wrap;
+      max-width: 33%;
     }
 
-    .footer__list-wrapper {
+    .footer__bottom .footer__bottom-text span {
+      padding-right: var(--spacing-smaller);
+      margin-bottom: var(--spacing-tiny);
+    }
+
+    .footer__wrapper {
       grid-column: content-left;
       text-align: left;
       grid-row: 1;
     }
-    .footer__list-wrapper.footer__list-wrapper--right {
+
+    .footer__wrapper.footer__wrapper--right {
       grid-column: content-right;
       text-align: right;
       grid-row: 1;
+    }
+
+    .footer-contact__header {
+      text-align: right;
+      margin-bottom: calc(var(--spacing-smaller) * 2 );
+    }
+
+    .footer-contact__link-list {
+      flex-direction: column;
+    }
+
+    .footer-contact__link-list a {
+      text-align: right;
+      margin: 0 0 var(--spacing-small) 0;
+      padding: 0 0 0 0;
+    }
+
+    .footer-contact__link-address {
+      flex-direction: column;
+    }
+
+    .footer-contact__link-address span {
+      padding-right: var(--spacing-tiny);
+      margin-bottom: var(--spacing-tiny);
     }
   }
 }
