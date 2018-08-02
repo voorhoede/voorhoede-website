e412f192bf92bac71349293d0d7a9b099c844dd0
Peter Goes <petergoes@gmail.com>
2018-08-02 14:57:33 +0200

    refactor(dato.config.js): add alternate urls to all data files

--- dato.config.js
+++ dato.config.js
@@ -5,31 +5,47 @@ const staticDir = 'src/client/static'
 const dataDir = `${staticDir}/data`
 const pagesDir = `${dataDir}/pages`
 
-const altLocales = { en: 'nl', nl: 'en' }
 const isPublished = (item) => item.published
 const hasBody = (item) => item.body
 
-const downloadPage = curry((dato, root, i18n, locales, page, contentFn, file) => {
+const downloadPage = curry((dato, root, i18n, locales, page, contentFn, _file) => {
   locales.forEach(locale => {
+    const file = typeof _file !== 'object'
+      ? locales.reduce((obj, locale) => ({ ...obj, [locale]: _file }), {})
+      : _file
+
     i18n.locale = locale
+
+    const alternateUris = locales.map(locale => ({
+      locale,
+      uri: i18n.withLocale(locale, () => `/${locale}/${file[locale]}`.replace('.json', '/').replace('index/', '')),
+    }))
     root.createDataFile(
-      `${pagesDir}/${locale}/${file}`,
+      `${pagesDir}/${locale}/${file[locale]}`,
       'json',
-      contentFn(dato[page], i18n, dato)
+      { ...contentFn(dato[page], i18n, dato), alternateUris }
     )
   })
 })
 
-const downloadCollection = curry((dato, root, i18n, locales, collection, contentFn, filterFn, folder) => {
+const downloadCollection = curry((dato, root, i18n, locales, collection, contentFn, filterFn, _folder) => {
   locales.forEach(locale => {
+    const folder = typeof _folder !== 'object'
+      ? locales.reduce((obj, locale) => ({ ...obj, [locale]: _folder }), {})
+      : _folder
+
     i18n.locale = locale
     dato[collection]
       .filter(filterFn)
       .forEach(item => {
+        const alternateUris = locales.map(locale => ({
+          locale,
+          uri: i18n.withLocale(locale, () => `/${locale}/${folder[locale]}/${item.slug}`),
+        }))
         root.createDataFile(
-          `${pagesDir}/${locale}/${folder}/${item.slug}.json`,
+          `${pagesDir}/${locale}/${folder[locale]}/${item.slug}.json`,
           'json',
-          contentFn(item, i18n, dato)
+          { ...contentFn(item, i18n, dato), alternateUris }
         )
       })
   })
@@ -41,20 +57,18 @@ module.exports = (dato, root, i18n) => {
     const getPageEn = downloadPage(dato, root, i18n, ['en'])
     const getCollection = downloadCollection(dato, root, i18n, i18n.availableLocales)
     const getCollectionEn = downloadCollection(dato, root, i18n, ['en'])
-    const getCollectionNl = downloadCollection(dato, root, i18n, ['nl'])
 
     getPage('home', homeData)('index.json')
     getPage('contact', contactData)('contact.json')
     getPage('eventOverview', eventsData)('academy.json')
-    getPage('team', teamData)('about-us.json')
+    getPage('team', teamData)({ en: 'about-us.json', nl: 'over-ons.json' })
     getPage('work', workData)('cases/index.json')
     getPageEn('blog', blogData)('blog/index.json')
 
-    // getCollection('services', servicesData, hasBody)('services')
+    getCollection('services', servicesData, hasBody)('services')
     getCollection('projects', portfolioData, isPublished)('cases')
+    getCollection('jobs', jobsData, isPublished)({ en:'jobs', nl:'vacatures' })
     getCollectionEn('blogPosts', blogPostsData, isPublished)('blog')
-    getCollectionEn('jobs', jobsData, isPublished)('jobs')
-    getCollectionNl('jobs', jobsData, isPublished)('vacatures')
 
 
   // Build should fail if dato dump fails
@@ -85,15 +99,11 @@ function homeData(data, i18n, dato) {
 }
 
 function contactData(data, i18n, dato) {
-  const altLocale = altLocales[i18n.locale]
   const picked = pick(['title', 'subtitle'], data)
   return {
     ...picked,
     description: data.social.description,
     keywords: data.keywords.split(',').map(keyword => keyword.trim()),
-    alternateUrls: i18n.withLocale(altLocale, () => ({
-      [altLocale]: `/${altLocale}/contact/`,
-    })),
     services: dato.home.services.map(pick(['slug', 'title'])),
     privacyStatementUrl: dato.home.privacyStatement.url(),
   }
--- src/client/pages/_locale/_aboutUs.vue
+++ src/client/pages/_locale/_aboutUs.vue
@@ -12,9 +12,12 @@ import getPageData from '../../lib/get-page-data'
 
 export default {
   async asyncData({ params }) {
-    const { locale } = params
-    const data = await getPageData({ uri: 'about-us', locale })
+    const { locale, aboutUs } = params
+    const data = await getPageData({ uri: aboutUs, locale })
     return data
+  },
+  validate ({ params }) {
+    return /^(about-us|over-ons)$/.test(params.aboutUs)
   },
 }
 </script>
