5417d48fd62bb0fef6c6b9c2caaa48e0a9a50067
Frank Warnaar <frank@voorhoede.nl>
2018-08-13 13:16:34 +0200

    refactor(page-header): page title & page-header-decoration combined to single component

---
+++ src/client/components/page-header/page-header.stories.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import { storiesOf } from '@storybook/vue'
+import { withReadme } from 'storybook-readme'
+import { withKnobs } from '@storybook/addon-knobs/vue'
+import VueI18n from 'vue-i18n'
+// import { action } from '@storybook/addon-actions'
+
+import '../app-core/index.css'
+
+import pageHeader from './page-header.vue'
+import README from './README.md'
+
+const stories = storiesOf('Components/Page Header', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(README))
+
+Vue.component('page-header', pageHeader)
+
+stories.add(
+  'Page Header',
+  () => ({
+    i18n: new VueI18n({ locale: 'en' }),
+    template: `
+      <div style="padding-bottom: 5rem; background: var(--bg-pastel);">
+        <page-header style="height: 100vh;" curly-bracket brick>
+          <template slot="title">
+            <span>We are De Voorhoede</span>
+          </template>
+          <template slot="subTitle">
+            <span>How can we help?</span>
+          </template>
+          <template slot="image">
+            <img style="max-width: 100%; max-height: 100%" src="https://www.datocms-assets.com/6068/1529572359-random-image.jpeg" alt="image">
+          </template>
+        </page-header>
+      </div>
+    `,
+  }),
+)
