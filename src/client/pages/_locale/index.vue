bc919d9a2f8005f89eeab3540c938d818208dd1e
Frank Warnaar <frank@voorhoede.nl>
2018-08-13 13:41:54 +0200

    feat(landingspage): services-list

--- src/client/pages/_locale/index.vue
+++ src/client/pages/_locale/index.vue
@@ -14,6 +14,12 @@
       </page-header>
       <scroll-highlighted-text :text-lines="usps"/>
     </div>
+    <section class="page-index__services grid">
+      <h2 class="page-index__services-title h1">{{ servicesHeader }}</h2>
+      <services-list
+        :services="services"
+      />
+    </section>
   </section>
 </template>
 
@@ -21,12 +27,14 @@
 import {
   PageHeader,
   ScrollHighlightedText,
+  ServicesList,
 } from '~/components'
 
 export default {
   components: {
     PageHeader,
-    ScrollHighlightedText
+    ScrollHighlightedText,
+    ServicesList,
   },
   layout: 'landing',
   asyncData() {
@@ -40,9 +48,45 @@ export default {
         'Start small and iterate fast',
         'to help you from an idea,',
         'to an amazing product.',
+      ],
+      servicesHeader: 'What can we do for you?',
+      services: [
+        {
+          title: 'We learn, we train',
+          subtitle: 'Kick-start your project',
+          characteristics: [
+            'Get a roadmap',
+            'Budget indication for your project',
+            'Test your idea using a prototype',
+            'With our input, you decide the next step',
+          ],
+          href: '/services/project-kick-start',
+        },
+        {
+          title: 'Need a team?',
+          subtitle: 'Your own agile team',
+          characteristics: [
+            'A dedicated team',
+            'Scrum master, developers and optional designers',
+            'On-site or at our office',
+            'You stay in charge',
+          ],
+          href: '/services/agile-team',
+        },
+        {
+          title: 'Need developers?',
+          subtitle: 'Front-end specialists',
+          characteristics: [
+            'Experts in JavaScript (frameworks), HTML and CSS',
+            'Who quickly get the hang of your project',
+            'Back-up of 20 colleagues',
+            'Immediately available',
+          ],
+          href: '/services/front-end-specialists',
+        }
       ]
     }
-  },
+  }
 }
 </script>
 
@@ -55,6 +99,15 @@ export default {
   padding: var(--spacing-larger) 0;
 }
 
+.page-index__services {
+  margin: var(--spacing-larger) 0;
+}
+
+.page-index__services-title {
+  margin-bottom: var(--spacing-large);
+  text-align: center;
+}
+
 @media (min-width: 720px) {
   .page-index .page-header {
     height: 100vh;
@@ -63,11 +116,28 @@ export default {
   .page-index .scroll-highlighted-text {
     padding: var(--spacing-big) 0;
   }
+
+  .page-index__services-title {
+    margin-bottom: var(--spacing-larger);
+  }
+
+  .page-index__services {
+    margin-top: var(--spacing-big);
+    margin-bottom: var(--spacing-bigger);
+  }
 }
 
 @media (min-width: 1100px) {
   .page-index .scroll-highlighted-text {
     padding: var(--spacing-bigger) 0;
+  }
+
+  .page-index__services-title {
+    margin-bottom: var(--spacing-big);
+  }
+
+  .page-index__services {
+    margin-top: var(--spacing-bigger);
   }
 }
 
