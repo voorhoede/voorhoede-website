0fa7e56f12897ba07109625789f028334bafbfd0
Casper Boutens <casper@voorhoede.nl>
2018-08-15 11:59:11 +0200

    wip(get-in-touch-form): add some styles and netlify

--- src/client/components/get-in-touch-form/get-in-touch-form.vue
+++ src/client/components/get-in-touch-form/get-in-touch-form.vue
@@ -2,26 +2,27 @@
   <section class="get-in-touch-form grid">
     <h2 class="get-in-touch-form__title h3">Let's discuss what we can do - together.</h2>
 
-    <form class="get-in-touch-form__form">
-      <label>
+    <form class="get-in-touch-form__form" method="POST" netlify>
+      <label class="get-in-touch-form__label">
         <span class="get-in-touch-form__label-text body-petite">My name is</span>
-        <input class="body" type="text" placeholder="Your name">
+        <input class="body" type="text" placeholder="Your name" name="name">
       </label>
-      <label>
+      <label class="get-in-touch-form__label">
         <span class="get-in-touch-form__label-text body-petite">You can email me at</span>
-        <input class="body" type="text" placeholder="Email address">
+        <input class="body" type="text" placeholder="Email address" name="email">
       </label>
-      <label>
+      <label class="get-in-touch-form__label">
         <span class="get-in-touch-form__label-text body-petite">You can call me on</span>
-        <input class="body" type="number" placeholder="Phone number">
+        <input class="body" type="number" placeholder="Phone number" name="number">
       </label>
-      <label>
+      <label class="get-in-touch-form__label">
         <span class="get-in-touch-form__label-text body-petite">My project is</span>
-        <input class="body" type="text" placeholder="Short summary of my project">
+        <input class="body" type="text" placeholder="Short summary of my project" name="explanation">
       </label>
       <app-button
-        label="Get in touch"
         class="get-in-touch-form__button"
+        label="Get in touch"
+        type="submit"
       />
     </form>
   </section>
@@ -38,8 +39,18 @@
 </script>
 
 <style>
+  .get-in-touch-form__label:first-of-type {
+    margin-top: 0;
+  }
+
   .get-in-touch-form__button {
     margin-top: var(--spacing-larger);
+  }
+
+  @media (max-width: 1099px) {
+    .get-in-touch-form__title {
+      margin-bottom: var(--spacing-medium);
+    }
   }
 
   @media (min-width: 1100px) {
