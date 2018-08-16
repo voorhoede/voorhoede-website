4df9e0875245c2dc89322b8adb48d6da169c9e3d
Declan Rek <declanrek@gmail.com>
2018-08-16 15:06:39 +0200

    refactor(store): do classic mode so we can import export/import it refactor(store): get default locale from process env

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,112 +1,110 @@
+import Vuex from 'vuex'
 import * as types from './mutation-types'
 import { getData } from '../lib/get-data'
 
-const state = () => ({
-  showGrid: false,
-  locales: ['nl', 'en'],
-  currentLocale: 'nl',
-  currentRoutePath: '/nl/',
-  menu: {
-    nl: [
-      {
-        title: 'Services',
-        slug: 'services',
-      },
-      {
-        title: 'Cases',
-        slug: 'cases',
-      },
-      {
-        title: 'Academy',
-        slug: 'academy',
-      },
-      {
-        title: 'Over Ons',
-        slug: 'over-ons',
-      },
-      {
-        title: 'Contact',
-        slug: 'contact',
-        button: true,
-      }
-    ],
-    en: [
-      {
-        title: 'Services',
-        slug: 'services',
-      },
-      {
-        title: 'Cases',
-        slug: 'cases',
-      },
-      {
-        title: 'Academy',
-        slug: 'academy',
-      },
-      {
-        title: 'About Us',
-        slug: 'about-us',
-      },
-      {
-        title: 'Contact',
-        slug: 'contact',
-        button: true,
-      }
-    ]
-  },
-})
+const createStore = () => {
+  return new Vuex.Store({
 
-const getters = {
-  alternateLocale: state => {
-    return state.locales.find(locale => locale !== state.currentLocale)
-  },
-  localizedMenuItems: state => state.menu[state.currentLocale]
-}
+    state: {
+      showGrid: false,
+      locales: ['nl', 'en'],
+      currentLocale: process.env.DEFAULT_LOCALE,
+      currentRoutePath: `/${process.env.DEFAULT_LOCALE}/`,
+      menu: {
+        nl: [
+          {
+            title: 'Services',
+            slug: 'services',
+          },
+          {
+            title: 'Cases',
+            slug: 'cases',
+          },
+          {
+            title: 'Academy',
+            slug: 'academy',
+          },
+          {
+            title: 'Over Ons',
+            slug: 'over-ons',
+          },
+          {
+            title: 'Contact',
+            slug: 'contact',
+            button: true,
+          }
+        ],
+        en: [
+          {
+            title: 'Services',
+            slug: 'services',
+          },
+          {
+            title: 'Cases',
+            slug: 'cases',
+          },
+          {
+            title: 'Academy',
+            slug: 'academy',
+          },
+          {
+            title: 'About Us',
+            slug: 'about-us',
+          },
+          {
+            title: 'Contact',
+            slug: 'contact',
+            button: true,
+          }
+        ]
+      },
+    },
+    getters: {
+      alternateLocale: state => {
+        return state.locales.find(locale => locale !== state.currentLocale)
+      },
+      localizedMenuItems: state => state.menu[state.currentLocale]
+    },
+    actions: {
+      async getData({ commit, state, getters }, { query, route }) {
 
-const actions = {
-  async getData({ commit, state, getters }, { query, route }) {
+        try {
+          const data = await getData({
+            query,
+            variables: {
+              locale: state.currentLocale,
+              alternateLocale: getters.alternateLocale,
+              slug: route.params.slug
+            }
+          })
 
-    try {
-      const data = await getData({
-        query,
-        variables: {
-          locale: state.currentLocale,
-          alternateLocale: getters.alternateLocale,
-          slug: route.params.slug
+          const alternateParentSlug = data.alternateParent ? `/${data.alternateParent.slug}` : ''
+          const alternateSlug = data.alternate ? `/${data.alternate.slug}` : ''
+          const url = `/${getters.alternateLocale}${alternateParentSlug}${alternateSlug}/`
+
+          commit(types.SET_ALTERNATE_URL, { url })
+          return data
+        } catch (e) {
+          console.error(e) // eslint-disable-line no-console
+          throw e
         }
-      })
-
-      const alternateParentSlug = data.alternateParent ? `/${data.alternateParent.slug}` : ''
-      const alternateSlug = data.alternate ? `/${data.alternate.slug}` : ''
-      const url = `/${getters.alternateLocale}${alternateParentSlug}${alternateSlug}/`
-
-      commit(types.SET_ALTERNATE_URL, { url })
-      return data
-    } catch (e) {
-      console.error(e) // eslint-disable-line no-console
-      throw e
+      }
+    },
+    mutations: {
+      [types.SHOW_GRID](state) {
+        state.showGrid = true
+      },
+      [types.HIDE_GRID](state) {
+        state.showGrid = true
+      },
+      [types.SET_CURRENT_LOCALE](state, { locale }) {
+        state.currentLocale = locale
+      },
+      [types.SET_ALTERNATE_URL](state, { url }) {
+        state.alternateUrl = url
+      }
     }
-  }
+  })
 }
 
-const mutations = {
-  [types.SHOW_GRID] (state) {
-    state.showGrid = true
-  },
-  [types.HIDE_GRID] (state) {
-    state.showGrid = true
-  },
-  [types.SET_CURRENT_LOCALE] (state, { locale }) {
-    state.currentLocale = locale
-  },
-  [types.SET_ALTERNATE_URL] (state, { url }) {
-    state.alternateUrl = url
-  }
-}
-
-export default {
-  state,
-  getters,
-  actions,
-  mutations
-}
+export default createStore
