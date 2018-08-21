df5c9a84664eba3a2b348d542bcf3f27ba167939
Casper Boutens <casper@voorhoede.nl>
2018-08-21 13:37:43 +0200

    cherry pick max width option

--- src/client/components/responsive-image/responsive-image.vue
+++ src/client/components/responsive-image/responsive-image.vue
@@ -1,6 +1,6 @@
 <template>
   <figure class="responsive-image">
-    <div class="responsive-image__sizer" :style="`max-width:${image.width}px;`">
+    <div class="responsive-image__sizer" :style="needsMaxWidth ? `max-width:${image.width}px;` : ''">
       <fixed-ratio class="responsive-image__canvas" :width="image.width" :height="image.height">
         <lazy-load>
           <picture class="responsive-image__picture">
@@ -49,6 +49,10 @@ export default {
       type: Number,
       default: 100,
     },
+    needsMaxWidth: {
+      type: Boolean,
+      default: true
+    }
   },
   data() {
     return {
