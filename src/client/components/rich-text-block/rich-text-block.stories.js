38bc68397a8742be576e81fb2e06b57c82e4ccd8
bruna <brunadafonseca@gmail.com>
2018-08-23 10:07:25 +0200

    Remove README

--- src/client/components/rich-text-block/rich-text-block.stories.js
+++ src/client/components/rich-text-block/rich-text-block.stories.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
-import { withReadme } from 'storybook-readme'
 import { withKnobs } from '@storybook/addon-knobs/vue'
 // import { action } from '@storybook/addon-actions'
 
 import '../app-core/index.css'
 
 import RichTextBlock from './rich-text-block.vue'
-import README from './README.md'
 
 const stories = storiesOf('Components/Rich Text Block', module)
   .addDecorator(withKnobs)
-  .addDecorator(withReadme(README))
 
 Vue.component('rich-text-block', RichTextBlock)
 
