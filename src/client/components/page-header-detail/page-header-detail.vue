1db936d4c687223c2bf615e08cddaccb63255056
bruna <brunadafonseca@gmail.com>
2018-08-28 15:29:47 +0200

    Add styles for header with brick and image

--- src/client/components/page-header-detail/page-header-detail.vue
+++ src/client/components/page-header-detail/page-header-detail.vue
@@ -8,73 +8,61 @@
       <h1 class="page-header__title hero">{{ title }}</h1>
       <h2 class="page-header__sub-title sub-title">{{ subTitle }}</h2>
     </div>
+    <scroll-to class="page-header__scroll-to"/>
   </header>
 </template>
 
 <script>
-export default {
-  props: {
-    seoTitle: {
-      type: String,
-      default: null,
+  import { ScrollTo } from '~/components'
+
+  export default {
+    components: {
+      ScrollTo,
     },
-    brick: {
-      type: Boolean,
-      default: false,
+    props: {
+      seoTitle: {
+        type: String,
+        default: null,
+      },
+      brick: {
+        type: Boolean,
+        default: false,
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      subTitle: {
+        type: String,
+        required: true
+      }
     },
-    curlyBracket: {
-      type: Boolean,
-      default: false,
+    computed: {
+      hasImage() {
+        return 'image' in this.$slots
+      },
     },
-    title: {
-      type: String,
-      required: true
-    },
-    subTitle: {
-      type: String,
-      required: true
-    }
-  },
-  computed: {
-    hasImage() {
-      return 'image' in this.$slots
-    },
-  },
-}
+  }
 </script>
 
 <style>
-.page-header {
-  padding-top: var(--app-header-height);
-  background: var(--bg-pastel);
-}
-
-.page-header__brick {
-  display: none;
-}
-
-.page-header__image img {
-  max-width: 100%;
-  max-height: 100%;
-}
-
-@supports (display: grid) {
   .page-header {
-    padding-top: 0;
-    grid-template-rows: var(--app-header-height) 1fr var(--spacing-large) calc(50vh - var(--spacing-large) - var(--spacing-larger)) var(--spacing-larger);
     position: relative;
+    grid-template-rows: var(--app-header-height) 1fr var(--spacing-large) 1fr var(--spacing-tiny);
+    padding-top: 0;
+    background: var(--bg-pastel);
   }
 
   .page-header__text {
     display: flex;
     flex-direction: column;
-    margin: var(--spacing-medium) 0;
     grid-column: content;
     grid-row-start: 2;
+    margin: var(--spacing-medium) 0;
   }
 
   .page-header__title {
-    margin-top: var(--spacing-large);
+    margin-top: var(--spacing-smaller);
     order: 2;
   }
 
@@ -83,15 +71,14 @@ export default {
   }
 
   .page-header__brick {
-    display: block;
-    background: var(--brand-yellow);
+    background-color: var(--brand-yellow);
     grid-column: page;
     grid-row-start: 3;
     grid-row-end: 6;
   }
 
   .page-header__image {
-    grid-column-start: 2;
+    grid-column-start: 8;
     grid-column-end: 19;
     grid-row-start: 4;
     grid-row-end: 5;
@@ -100,58 +87,73 @@ export default {
     align-items: flex-end;
   }
 
+  .page-header__image img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .page-header__scroll-to.scroll-to {
+    position: absolute;
+    grid-row-start: 5;
+  }
+
   @media screen and (min-width: 520px) {
     .page-header__image {
       grid-column: content;
-      grid-column-start: 4;
-      grid-column-end: 16;
       justify-content: center;
     }
   }
 
   @media screen and (min-width: 720px) {
     .page-header {
-      grid-template-rows: calc(var(--app-header-height) + var(--spacing-big)) 1fr var(--spacing-tiny) calc(var(--spacing-larger) - var(--spacing-tiny));
+      grid-template-rows: var(--app-header-height) 1fr .3fr .3fr 1fr calc(var(--spacing-larger) - var(--spacing-tiny));
     }
 
     .page-header__text {
-      margin: var(--spacing-medium) 0;
-      grid-column: content;
-      grid-row-start: 2;
-      grid-column-start: 2;
-      grid-column-end: 12;
+      grid-column: page-left;
+      grid-column-start: 4;
+      grid-column-end: 16;
+      grid-row-end: 4;
     }
 
     .page-header__brick {
-      height: 100%;
       grid-column: page-right;
       grid-row-start: 1;
       grid-row-end: -1;
     }
 
     .page-header__image {
-      grid-column-start: 13;
-      grid-column-end: 33;
-      grid-row-start: 2;
-      grid-row-end: 3;
+      grid-column: page-right;
+      grid-column-end: 49;
+      grid-row-start: 4;
+      grid-row-end: 6;
       justify-content: flex-end;
+    }
+
+    .page-header__scroll-to.scroll-to {
+      position: absolute;
+      bottom: -160px; /* magic number to positon scroll with case-meta text */
+      grid-row-start: 7;
     }
   }
 
   @media screen and (min-width: 1100px) {
     .page-header {
-      grid-template-rows: calc(var(--app-header-height) + var(--spacing-larger)) 1fr var(--spacing-tiny) calc(var(--spacing-larger) - var(--spacing-tiny));
+      grid-template-rows: var(--app-header-height) 1fr .6fr var(--spacing-large);
     }
 
     .page-header__text {
-      grid-column-start: 4;
       grid-column-end: 21;
+      grid-row-end: 3;
     }
-    
+
     .page-header__image {
-      grid-column-start: 22;
-      grid-column-end: 49;
+      grid-row-start: 3;
+      grid-row-end: 4;
+    }
+
+    .page-header__scroll-to.scroll-to {
+      bottom: -163px;
     }
   }
-}
 </style>
