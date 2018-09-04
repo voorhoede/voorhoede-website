7d7d5912f13b4e0e027d98f299197eb34640def4
Casper Boutens <casper@voorhoede.nl>
2018-09-04 17:10:46 +0200

    feat(services-page): add list and form components

--- src/client/pages/_locale/services/index.vue
+++ src/client/pages/_locale/services/index.vue
@@ -1,15 +1,49 @@
 <template>
-  <section class="page-services">
-    <div>
-      <pre>{{ page }}</pre>
-    </div>
-  </section>
+  <main class="page-services grid">
+    <services-list :services="page.services"/>
+    <get-in-touch-form
+      :title="page.getInTouchTitle"
+      :name-label="page.getInTouchForm.nameLabel"
+      :name-placeholder="page.getInTouchForm.namePlaceholder"
+      :email-label="page.getInTouchForm.emailLabel"
+      :email-placeholder="page.getInTouchForm.emailPlaceholder"
+      :phone-label="page.getInTouchForm.phoneLabel"
+      :phone-placeholder="page.getInTouchForm.phonePlaceholder"
+      :summary-label="page.getInTouchForm.summaryLabel"
+      :summary-placeholder="page.getInTouchForm.summaryPlaceholder"
+      :cta-label="page.getInTouchForm.ctaButtonLabel"
+    />
+  </main>
 </template>
 
 <script>
+  import { ServicesList, GetInTouchForm } from '~/components'
+
   export default {
+    components: {
+      ServicesList,
+      GetInTouchForm,
+    },
     async asyncData({ store, route }) {
       return await store.dispatch('getData', { route })
     },
   }
 </script>
+
+<style>
+  .page-services > * {
+    margin-bottom: var(--spacing-larger);
+  }
+
+  @media (min-width: 720px) {
+    .page-services > * {
+      margin-bottom: var(--spacing-big);
+    }
+  }
+
+  @media (min-width: 1100px) {
+    .page-services > * {
+      margin-bottom: var(--spacing-bigger);
+    }
+  }
+</style>
