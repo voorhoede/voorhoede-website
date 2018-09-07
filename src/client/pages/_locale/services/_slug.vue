cf7e8eddc1d0265122c0e931f9f1261d556a462f
Casper Boutens <casper@voorhoede.nl>
2018-09-07 13:48:46 +0200

    feat(service-page): create component

--- src/client/pages/_locale/services/_slug.vue
+++ src/client/pages/_locale/services/_slug.vue
@@ -1,13 +1,63 @@
 <template>
-  <section class="page-services">
-    <pre>{{ page }}</pre>
-  </section>
+  <div class="grid page-service">
+    <page-header-detail
+      :title="page.subtitle"
+      :sub-title="page.title"
+      :image="page.headerIllustration"
+    />
+    <div class="page-service__overview">
+      <ul>
+        <li class="page-service__overview-item" v-for="item in page.items" :key="item.title">
+          <generic-text-block
+            :title="item.title"
+            :body="item.body"
+            :image="item.image"
+          />
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
-
-<script>
-  export default {
-    async asyncData({ store, route }) {
-      return await store.dispatch('getData', { route })
-    },
+ <script>
+import { PageHeaderDetail, GenericTextBlock } from '~/components'
+ export default {
+  components: {
+    PageHeaderDetail,
+    GenericTextBlock
+  },
+  async asyncData({ store, route }) {
+    return await store.dispatch('getData', { route })
   }
+}
 </script>
+ <style>
+  .page-service .page-header {
+    grid-column: page;
+    margin-bottom: var(--spacing-large);
+  }
+
+  .page-service__overview-item {
+    margin-bottom: var(--spacing-large);
+  }
+
+  @media (min-width: 720px) {
+    .page-service {
+      background: var(--bg-pastel);
+    }
+
+    .page-service__overview {
+      grid-column-start: 2;
+      grid-column-end: -2;
+      background: var(--white);
+      padding: var(--spacing-large) var(--spacing-larger);
+    }
+  }
+
+  @media (min-width: 1100px) {
+    .page-service__overview {
+      grid-column-start: 6;
+      grid-column-end: -6;
+      padding: var(--spacing-big) var(--spacing-bigger);
+    }
+  }
+ </style>
