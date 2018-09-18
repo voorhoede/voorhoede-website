287dadef85368dda110557434f41ca739fdcb132
Thadee Trompetter <thadee@voorhoede.nl>
2018-09-18 16:14:32 +0200

    remove references to contact page form readme

--- src/client/components/contact-page-form/contact-page-form.stories.js
+++ src/client/components/contact-page-form/contact-page-form.stories.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
-import { withReadme } from 'storybook-readme'
 import { withKnobs } from '@storybook/addon-knobs/vue'
-// import { action } from '@storybook/addon-actions'
 
 import '../app-core/index.css'
 
 import ContactPageForm from './contact-page-form.vue'
-import README from './README.md'
 
 const stories = storiesOf('Components/Contact Page Form', module)
   .addDecorator(withKnobs)
-  .addDecorator(withReadme(README))
 
 Vue.component('contact-page-form', ContactPageForm)
 
