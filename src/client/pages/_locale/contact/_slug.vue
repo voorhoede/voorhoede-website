59625d8851673239a950df797eefc03f27c57d36
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2018-10-02 15:44:52 +0200

    feat(contact-confirmation): try to get info from dato

---
+++ src/client/pages/_locale/contact/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ store, route }) {
+      return await store.dispatch('getData', { route })
+    }
+  }
+</script>
