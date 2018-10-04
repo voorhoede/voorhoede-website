4a284dcc082f1ec9814a5ed425f60b64a640088c
Vincent van Dijck <vincent@voorhoede.nl>
2018-10-04 15:38:52 +0200

    feat(rich-text-block) improved table styles

--- src/client/components/rich-text-block/rich-text-block.vue
+++ src/client/components/rich-text-block/rich-text-block.vue
@@ -23,35 +23,34 @@
   }
 
   .rich-text table {
-    margin: 0 auto;
-    max-width: 500px;
     margin-bottom: var(--spacing-medium);
   }
 
   .rich-text td {
-    padding: var(--spacing-small) 0;
+    padding: var(--spacing-small) var(--spacing-tiny);
     text-align: center;
   }
 
   .rich-text tr {
-    border-bottom: 1px solid rgba(0, 0, 0, .1);
+    border-bottom: 1px solid var(--very-dim);
   }
 
   .rich-text tr:first-child {
-    font-size: .75rem; /* 12px */
-    font-weight: 700;
-    line-height: 1.3333333333; /* 16px */
-    text-transform: uppercase;
-    letter-spacing: .1375rem; /* 2.2px */
-    color: var(--html-blue);
     border-bottom: 2px solid var(--very-dim);
+    color: var(--html-blue);
+  }
+
+  .rich-text tr:first-child td {
+    padding: var(--spacing-medium) var(--spacing-tiny);
+    font-weight: 700;
+    vertical-align: bottom;
   }
 
   .rich-text ul,
   .rich-text ol {
-    font-size: 1rem;
-    margin-left: var(--spacing-medium);
     margin-bottom: var(--spacing-medium);
+    margin-left: var(--spacing-medium);
+    font-size: 1rem;
   }
 
   .rich-text li {
@@ -61,12 +60,6 @@
   @media (min-width: 720px) {
     .rich-text p:not(:last-child) {
       margin-bottom: var(--spacing-large);
-    }
-
-    .rich-text tr:first-child {
-      font-size: .8125rem; /* 13px */
-      line-height: 1.3076923077; /* 17px */
-      letter-spacing: .15rem; /* 2.4px */
     }
 
     .rich-text ul,
