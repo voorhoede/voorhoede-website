589df6453132a00cc40e097c660121d3432a6bab
Jesse Eikema <jesseeikema@MacBook-Pro-van-Jesse.local>
2018-10-23 17:24:13 +0200

    feat(jobs): added jobs overview page

--- src/client/pages/_locale/jobs/index.vue
+++ src/client/pages/_locale/jobs/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <section class="page-cases">
-    <pre>{{ page }}</pre>
+  <section class="page-jobs">
+    <page-header
+      :title="page.title"
+      :text="page.subtitle"
+      :image="page.headerIllustration"
+    />
+    <div class="grid">
+      <ul class="page-jobs__overview">
+        <li 
+          class="page-about-us__jobs-list" 
+          v-for="item in page.jobs" 
+          :key="item.title">
+          <jobs-excerpt
+            class="page-about-us__jobs-list-item"
+            :title="item.title"
+            :description="item.description"
+            :label="item.callToActionLabel"
+            :url="item.url"
+            :image="item.jobImage"
+            :slug="item.slug"
+          />
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+  import { PageHeader, JobsExcerpt } from '~/components'
+
   export default {
+    components: {
+      PageHeader,
+      JobsExcerpt
+    },
+    computed: {
+      ...mapState(['currentLocale']),
+    },
     async asyncData({ store, route }) {
       return await store.dispatch('getData', { route })
     },
@@ -21,3 +53,17 @@
     }
   }
 </script>
+
+<style>
+.page-jobs__overview {
+  grid-column: content;
+  margin: var(--spacing-large) 0;
+}
+
+@media (min-width: 1100px) {
+  .page-jobs__overview {
+    grid-column: 6 / -12;
+  }
+}
+</style>
+
