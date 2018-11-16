2757290ab9c084910944de49f07f58fe1e2481a4
Jurgen Beliën <jurgen@voorhoede.nl>
2018-11-16 13:57:01 +0100

    fix(curly-bracket): position on wide breakpoint

--- src/client/components/curly-bracket/curly-bracket.vue
+++ src/client/components/curly-bracket/curly-bracket.vue
@@ -28,15 +28,15 @@ export default {
 <style>
   .curly-bracket {
     margin-top: var(--spacing-bigger);
-    grid-column-end: 4;
+    grid-column-start: 1;
+    grid-column-end: 5;
     grid-row-start: 1;
     position: relative;
-    /* overflow: hidden; */
   }
 
   .curly-bracket--right {
-    grid-column-start: -4;
-    grid-column-end: -2;
+    grid-column-start: -5;
+    grid-column-end: -1;
     grid-row-start: 2;
     grid-row-end: 3;
   }
@@ -46,44 +46,22 @@ export default {
     margin: auto;
     top: -2vh;
     bottom: -2vh;
-    right: -2vh;
-    /* max-height: calc(100% + 4vh); */
+    left: 0;
+    max-height: calc(100% + 4vh);
     width: 100%;
+    max-width: auto;
     /* max-height: 320px; */
   }
 
   .curly-bracket--right .curly-bracket__image {
     transform: scale(-1,1);
     transform-origin: center center;
-    left: 0;
-    right: unset;
+    left: unset;
+    right: 0;
   }
-  /* @media (min-width: 540px) {
-    .curly-bracket {
-      max-height: none;
-      overflow: visible;
-      grid-column-start: 3;
-      grid-column-end: 5;
-    }
-
-    .curly-bracket--right {
-      grid-column-start: -4;
-      grid-column-end: -1;
-    }
-
-    .curly-bracket__image {
-      top: 0;
-      height: auto;
-      width: 80%;
-    }
-  } */
   @media (min-width: 720px) {
     .curly-bracket {
       margin-top: 0;
-      /* margin-top: 0;
-      position: absolute;
-      top: calc(-1 * var(--spacing-medium));
-      bottom: calc(-1 * var(--spacing-medium)); */
       grid-column-start: 2;
       grid-column-end: 6;
     }
@@ -94,13 +72,20 @@ export default {
       grid-column-end: -2;
     }
 
-    .curly-bracket__image {
-
-    }
-
     .curly-bracket--right .curly-bracket__image {
       right: 0;
       left: auto;
+    }
+  }
+  @media (min-width: 1100px) {
+    .curly-bracket {
+      grid-column-start: 3;
+      grid-column-end: 8;
+    }
+
+    .curly-bracket--right {
+      grid-column-start: -8;
+      grid-column-end: -3;
     }
   }
 </style>
