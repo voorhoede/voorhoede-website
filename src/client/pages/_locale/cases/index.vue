35af07f24da4886b4e36982f7289e050e71ecd54
Jesse Eikema <jesseeikema@MacBook-Pro-van-Jesse.local>
2018-11-20 17:32:53 +0100

    Revert "feat(case-overview): added fix for case overview"
    This reverts commit 691be555069a91789a732b7bc0753aa9ba50440b.

--- src/client/pages/_locale/cases/index.vue
+++ src/client/pages/_locale/cases/index.vue
@@ -69,136 +69,36 @@
   margin-bottom: var(--spacing-big);
 }
 
-@media (min-width: 540px) {
-  .page-case__overview {
-    display: grid;
-    grid-column: var(--grid-content);
-    grid-template-columns: 1fr 1fr;
-    grid-gap: var(--spacing-medium);
+@supports (display: grid) {
+  @media (min-width: 540px) {
+    .page-case__overview {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: var(--spacing-medium);
+    }
+
+    .page-case__overview .case-excerpt {
+      margin-bottom: var(--spacing-larger);
+    }
   }
 
-  .page-case__overview .case-excerpt {
-    margin-bottom: var(--spacing-larger);
+  @media (min-width: 720px) {
+    .page-case__overview .case-excerpt {
+      margin-bottom: 0;
+    }
+
+    .page-case__overview {
+      grid-gap: var(--spacing-large);
+      margin: var(--spacing-larger) 0;
+    }
   }
 
-  .page-case__overview-item:nth-child(2n - 1) {
-    grid-column: 1;
-  }
-
-  .page-case__overview-item:nth-child(2n) {
-    grid-column: 2;
-  }
-
-  .page-case__overview-item:nth-child(1),
-  .page-case__overview-item:nth-child(2) {
-    grid-row: 1;
-  }
-
-  .page-case__overview-item:nth-child(3),
-  .page-case__overview-item:nth-child(4) {
-    grid-row: 2;
-  }
-
-  .page-case__overview-item:nth-child(5),
-  .page-case__overview-item:nth-child(6) {
-    grid-row: 3;
-  }
-
-  .page-case__overview-item:nth-child(7),
-  .page-case__overview-item:nth-child(8) {
-    grid-row: 4;
-  }
-
-  .page-case__overview-item:nth-child(9),
-  .page-case__overview-item:nth-child(10) {
-    grid-row: 5;
-  }
-  
-  .page-case__overview-item:nth-child(11),
-  .page-case__overview-item:nth-child(12) {
-    grid-row: 6;
-  }
-  
-  .page-case__overview-item:nth-child(13),
-  .page-case__overview-item:nth-child(14) {
-    grid-row: 7;
-  }
-
-  .page-case__overview-item:nth-child(15),
-  .page-case__overview-item:nth-child(16) {
-    grid-row: 8;
-  }
-  
-  .page-case__overview-item:nth-child(17),
-  .page-case__overview-item:nth-child(18) {
-    grid-row: 9;
-  }
-}
-
-@media (min-width: 720px) {
-  .page-case__overview .case-excerpt {
-    margin-bottom: 0;
-  }
-
-  .page-case__overview {
-    grid-gap: var(--spacing-large);
-    margin: var(--spacing-larger) 0;
-  }
-}
-
-@media (min-width: 1200px) {
-  .page-case__overview {
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-column-start: 4;
-    grid-column-end: 48;
-  }
-  
-  .page-case__overview-item:nth-child(3n - 2) {
-    grid-column: 1;
-  }
-
-  .page-case__overview-item:nth-child(3n - 1) {
-    grid-column: 2;
-  }
-
-  .page-case__overview-item:nth-child(3n) {
-    grid-column: 3;
-  }
-
-  .page-case__overview-item:nth-child(1),
-  .page-case__overview-item:nth-child(2),
-  .page-case__overview-item:nth-child(3) {
-    grid-row: 1;
-  }
-
-  .page-case__overview-item:nth-child(4),
-  .page-case__overview-item:nth-child(5),
-  .page-case__overview-item:nth-child(6) {
-    grid-row: 2;
-  }
-
-  .page-case__overview-item:nth-child(7),
-  .page-case__overview-item:nth-child(8),
-  .page-case__overview-item:nth-child(9) {
-    grid-row: 3;
-  }
-
-  .page-case__overview-item:nth-child(10),
-  .page-case__overview-item:nth-child(11),
-  .page-case__overview-item:nth-child(12) {
-    grid-row: 4;
-  }
-
-  .page-case__overview-item:nth-child(13),
-  .page-case__overview-item:nth-child(14),
-  .page-case__overview-item:nth-child(15) {
-    grid-row: 5;
-  }
-  
-  .page-case__overview-item:nth-child(16),
-  .page-case__overview-item:nth-child(17),
-  .page-case__overview-item:nth-child(18) {
-    grid-row: 6;
+  @media (min-width: 1200px) {
+    .page-case__overview {
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-column-start: 4;
+      grid-column-end: -4;
+    }
   }
 }
 </style>
