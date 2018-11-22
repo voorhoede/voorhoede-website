a8fe71eaa2edbc133929273194e41415c1d26822
Jesse Eikema <jesseeikema@MacBook-Pro-van-Jesse.local>
2018-11-22 10:33:45 +0100

    feat(storytelling): added flex to component

--- src/client/components/storytelling-section/storytelling-section.vue
+++ src/client/components/storytelling-section/storytelling-section.vue
@@ -1,6 +1,8 @@
 <template>
   <aside class="storytelling-section grid">
-    <h2 class="storytelling-section__title h3">{{ title }}</h2>
+    <div class="storytelling-section__header">
+      <h2 class="storytelling-section__title h3">{{ title }}</h2>
+    </div>
     <section
       class="storytelling-section__item"
       v-for="item in items"
@@ -52,8 +54,15 @@
 
 <style>
   @import '../app-core/variables.css';
+  
+  :root {
+    --section-max-width: 640px;
+  }
 
   .storytelling-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     background-color: var(--paper);
     grid-row-gap: var(--spacing-larger);
     grid-column: var(--grid-content);
@@ -72,7 +81,22 @@
     padding: var(--spacing-medium);
   }
 
+  .storytelling-section__item {
+    margin-bottom: var(--spacing-larger);
+    width: 100%;
+  }
+
+  .storytelling-section__header {
+    text-align: left;
+    width: 100%;
+    margin-bottom: var(--spacing-larger);
+  }
+
   @media (min-width: 720px) {
+    .storytelling-section__header {
+      max-width: var(--section-max-width);
+    }
+ 
     .storytelling-section__title {
       grid-column-start: 5;
     }
@@ -84,6 +108,7 @@
       grid-column-gap: var(--spacing-small);
       align-items: center;
       grid-template-columns: repeat(3, 1fr);
+      max-width: var(--section-max-width);
     }
 
     .storytelling-section__image {
