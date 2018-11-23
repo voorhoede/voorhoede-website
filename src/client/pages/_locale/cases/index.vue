d1fe1897c15877141836ee4b6ab5d8e4762bb32c
Jesse Eikema <jesseeikema@MacBook-Pro-van-Jesse.local>
2018-11-23 15:13:08 +0100

    refactor(case-overview): added flex instead of grid

--- src/client/pages/_locale/cases/index.vue
+++ src/client/pages/_locale/cases/index.vue
@@ -62,143 +62,41 @@
 @import '../../../components/app-core/variables.css';
 
 .page-case__overview {
+  display: flex;
+  flex-direction: column;
   margin: var(--spacing-large) 0 0 0;
 }
 
-.page-case__overview .case-excerpt {
+.page-case__overview-item {
+  flex-basis: 100%;
   margin-bottom: var(--spacing-big);
 }
 
 @media (min-width: 540px) {
   .page-case__overview {
-    display: grid;
-    grid-column: var(--grid-content);
-    grid-template-columns: 1fr 1fr;
-    grid-gap: var(--spacing-medium);
+    flex-flow: row wrap;
   }
 
-  .page-case__overview .case-excerpt {
+  .page-case__overview-item {
+    flex-basis: calc(50% - (var(--spacing-smaller) * 2));
+    margin-left: var(--spacing-smaller);
+    margin-right: var(--spacing-smaller);
     margin-bottom: var(--spacing-larger);
-  }
-
-  .page-case__overview-item:nth-child(2n - 1) {
-    grid-column: 1;
-  }
-
-  .page-case__overview-item:nth-child(2n) {
-    grid-column: 2;
-  }
-
-  .page-case__overview-item:nth-child(1),
-  .page-case__overview-item:nth-child(2) {
-    grid-row: 1;
-  }
-
-  .page-case__overview-item:nth-child(3),
-  .page-case__overview-item:nth-child(4) {
-    grid-row: 2;
-  }
-
-  .page-case__overview-item:nth-child(5),
-  .page-case__overview-item:nth-child(6) {
-    grid-row: 3;
-  }
-
-  .page-case__overview-item:nth-child(7),
-  .page-case__overview-item:nth-child(8) {
-    grid-row: 4;
-  }
-
-  .page-case__overview-item:nth-child(9),
-  .page-case__overview-item:nth-child(10) {
-    grid-row: 5;
-  }
-  
-  .page-case__overview-item:nth-child(11),
-  .page-case__overview-item:nth-child(12) {
-    grid-row: 6;
-  }
-  
-  .page-case__overview-item:nth-child(13),
-  .page-case__overview-item:nth-child(14) {
-    grid-row: 7;
-  }
-
-  .page-case__overview-item:nth-child(15),
-  .page-case__overview-item:nth-child(16) {
-    grid-row: 8;
-  }
-  
-  .page-case__overview-item:nth-child(17),
-  .page-case__overview-item:nth-child(18) {
-    grid-row: 9;
   }
 }
 
 @media (min-width: 720px) {
-  .page-case__overview .case-excerpt {
-    margin-bottom: 0;
-  }
-
   .page-case__overview {
-    grid-gap: var(--spacing-large);
     margin: var(--spacing-larger) 0;
   }
 }
 
-@media (min-width: 1200px) {
-  .page-case__overview {
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-column-start: 4;
-    grid-column-end: 48;
-  }
-  
-  .page-case__overview-item:nth-child(3n - 2) {
-    grid-column: 1;
-  }
-
-  .page-case__overview-item:nth-child(3n - 1) {
-    grid-column: 2;
-  }
-
-  .page-case__overview-item:nth-child(3n) {
-    grid-column: 3;
-  }
-
-  .page-case__overview-item:nth-child(1),
-  .page-case__overview-item:nth-child(2),
-  .page-case__overview-item:nth-child(3) {
-    grid-row: 1;
-  }
-
-  .page-case__overview-item:nth-child(4),
-  .page-case__overview-item:nth-child(5),
-  .page-case__overview-item:nth-child(6) {
-    grid-row: 2;
-  }
-
-  .page-case__overview-item:nth-child(7),
-  .page-case__overview-item:nth-child(8),
-  .page-case__overview-item:nth-child(9) {
-    grid-row: 3;
-  }
-
-  .page-case__overview-item:nth-child(10),
-  .page-case__overview-item:nth-child(11),
-  .page-case__overview-item:nth-child(12) {
-    grid-row: 4;
-  }
-
-  .page-case__overview-item:nth-child(13),
-  .page-case__overview-item:nth-child(14),
-  .page-case__overview-item:nth-child(15) {
-    grid-row: 5;
-  }
-  
-  .page-case__overview-item:nth-child(16),
-  .page-case__overview-item:nth-child(17),
-  .page-case__overview-item:nth-child(18) {
-    grid-row: 6;
+@media (min-width: 1100px) {
+  .page-case__overview-item {
+    flex-basis: calc(33% - (var(--spacing-smaller) * 2));
+    margin-bottom: var(--spacing-larger);
+    margin-left: var(--spacing-smaller);
+    margin-right: var(--spacing-smaller);
   }
 }
 </style>
