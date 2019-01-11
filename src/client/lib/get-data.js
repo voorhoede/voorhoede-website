19ccf7bf40b80156583897cae07f664ad2a31965
Frank Warnaar <frank@voorhoede.nl>
2019-01-11 13:26:39 +0100

    feat(get-data): querying working server side

--- src/client/lib/get-data.js
+++ src/client/lib/get-data.js
@@ -1,54 +1,46 @@
 const path = require('path')
 const fetch = require('node-fetch')
 
-const clientPath = './src/client'
 const locales = ['nl', 'en']
 const layoutRegex = new RegExp(`(${locales.join('|')})/layouts/`)
 const localesRegex = new RegExp(`(${locales.join('|')})`)
 
 export function getData(route, variables) {
   let queryPath
+  const clientPath = process.client ? '..' : 'src/client'
 
   if (route.match(layoutRegex)) {
-    queryPath = clientPath + '/layouts' + route.replace(layoutRegex, '') + '.query.graphql'
+    queryPath = path.join(clientPath, 'layouts', route.replace(layoutRegex, '') + '.query.graphql')
   } else {
-    queryPath = clientPath + route.replace(localesRegex, '_locale') + 'index.query.graphql'
+    queryPath = path.join(clientPath, 'pages', route.replace(localesRegex, '_locale'), 'index.query.graphql')
   }
 
-  console.log({ queryPath });
+  const query = process.client ? require(queryPath) : require('fs').readFileSync(queryPath, 'utf8')
 
-  // const query = require(queryPath)
-
-  // const query = require('../')
-  console.log({ route, queryPath })
-  const filepath = path.join('/data', route, 'index.json')
-  if (process.client) {
-    // On client load over http
-    return fetch(filepath).then(res => res.json())
+  if (process.env.NODE_ENV !== 'production') {
+    return fetch(
+      'https://graphql.datocms.com/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${process.env.DATO_API_TOKEN}`,
+        },
+        body: JSON.stringify({ query, variables })
+      }
+    )
+      .then(res => res.json())
+      .then(res => res.data)
   } else {
-    // On server load from file system
-    const data = JSON.parse(require('fs').readFileSync(`src/client/static${filepath}`, 'utf8'))
-    return Promise.resolve(data)
+    const filepath = path.join('/data', route, 'index.json')
+    if (process.client) {
+      // On client load over http
+      return fetch(filepath).then(res => res.json())
+    } else {
+      // On server load from file system
+      const data = JSON.parse(require('fs').readFileSync(`src/client/static${filepath}`, 'utf8'))
+      return Promise.resolve(data)
+    }
   }
-  // if (process.env.NODE_ENV === 'production' || route !== '/nl/') {
-  // } else {
-  //   const query =
-  //   return fetch(
-  //     'https://graphql.datocms.com/',
-  //     {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'Accept': 'application/json',
-  //         'Authorization': `Bearer ${process.env.DATO_API_TOKEN}`,
-  //       },
-  //       body: JSON.stringify({ query, variables })
-  //     }
-  //   )
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       console.log(res)
-  //       return res.data
-  //     })
-  // }
 }
