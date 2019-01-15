be187e6b520ac33dcfacb197abac3b105d7cb57b
Jurgen Beliën <jurgen@voorhoede.nl>
2019-01-15 09:32:36 +0100

    feat(dato.config.js): add basic redirects

--- dato.config.js
+++ dato.config.js
@@ -10,14 +10,17 @@ dotenv.config()
 
 const staticDir = 'src/client/static'
 const dataDir = `${staticDir}/data`
+let defaultLocale = process.env.DEFAULT_LOCALE
 let locales = []
 
 module.exports = (dato, root, i18n) => {
   locales = i18n.availableLocales
 
-  fs.writeFileSync(`${__dirname}/${staticDir}/_redirects`, redirectsToText(dato.redirects), 'utf8')
-
   root.createDataFile(`${dataDir}/locales.json`, 'json', localesToJson(locales))
+  defaultLocale = defaultLocale || locales[0]
+
+  fs.writeFileSync(`${__dirname}/${staticDir}/_redirects`, redirectsToText(dato.redirects, locales, defaultLocale), 'utf8')
+
 
   locales.forEach(locale => {
     i18n.withLocale(locale, () => {
@@ -38,11 +41,14 @@ function localesToJson (locales) {
 /**
  * Write redirects to text with 1 redirect per line
  * @see https://www.netlify.com/docs/redirects/
+ *
  */
-function redirectsToText (redirects) {
-  return redirects
-    .map(redirect => `${redirect.from} ${redirect.to} ${redirect.statusCode}`)
-    .join('\n')
+function redirectsToText (redirects, locales, defaultLocale) {
+  const redirectToDefaultLocale = `/ /${defaultLocale}/ 301`
+  const redirectRulesFromCms = redirects
+    .map(redirect => `${redirect.from} ${redirect.to} ${redirect.httpStatusCode}`)
+  const redirectRules404s = locales.map(locale => `/${locale}/* /${locale}/404/ 404`)
+  return [redirectToDefaultLocale, ...redirectRulesFromCms, ...redirectRules404s].join('\n')
 }
 
 /**
