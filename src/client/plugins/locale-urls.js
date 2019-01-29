dc1304e4f4a4a119c1a510f255991b5ff9720482
Jurgen Beliën <jurgen@voorhoede.nl>
2019-01-29 14:09:06 +0100

    refactor(locale-urls): use methods

--- src/client/plugins/locale-urls.js
+++ src/client/plugins/locale-urls.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 const enforceTrailingSlash = url => (url.endsWith('/') ? url : `${url}/`)
 
 Vue.mixin({
-  created() {
+  methods: {
     /**
      * Extends Nuxt I18n's localeUrl with an enforced trailing slash
      * @see https://nuxt-community.github.io/nuxt-i18n/basic-usage.html#nuxt-link
@@ -12,17 +12,17 @@ Vue.mixin({
      * @param {object} options.params   route parameters
      * @param {string} [locale]
      */
-    this.localeUrl = (options, locale) => {
+    localeUrl (options, locale) {
       const url = this.localePath(options, locale)
       return enforceTrailingSlash(url)
-    }
+    },
 
     /**
      * Extends Nuxt I18n's switchLocaleUrl with an enforced trailing slash
      * @see https://nuxt-community.github.io/nuxt-i18n/lang-switcher.html
      * @param {string} locale
      */
-    this.switchLocaleUrl = locale => {
+    switchLocaleUrl (locale) {
       const url = this.switchLocalePath(locale)
       return enforceTrailingSlash(url)
     }
