8e2de22681b269a9041fb1d53c1a1d1c14087977
Jesse Eikema <jesse@voorhoede.nl>
2019-01-30 17:16:31 +0100

    feat(toc): add sticky toc

--- src/client/components/table-of-content/table-of-content.vue
+++ src/client/components/table-of-content/table-of-content.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="table-of-content">
-    <h3 class="body-big font-html-blue">Table of content</h3>
-    <ul class="flat-list">
-      <li 
-        class="table-of-content__list-item"
-        v-for="item in items" :key="item.title"> 
-        <a 
-          :href="`#${item.title.replace(/\s+/g, '').toLowerCase()}`"
-          v-if="item.title" 
-          class="body">
-          {{ item.title }}
-        </a>
-      </li>
-    </ul>
+    <div :class="{ 'table-of-content--sticky' : this.sticky }">
+      <h3 class="body-big font-html-blue">Table of content</h3>
+      <ul class="flat-list">
+        <li 
+          class="table-of-content__list-item"
+          v-for="item in items" :key="item.title"> 
+          <a 
+            :href="`#${item.title.replace(/\s+/g, '').toLowerCase()}`"
+            v-if="item.title" 
+            class="body">
+            {{ item.title }}
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import debounce from '../../lib/debounce-helper'
+
   export default {
     props: {
       items: {
@@ -24,16 +28,39 @@
         required: true,
       }
     },
-    computed: {},
+    data() {
+      return {
+        sticky: null,
+      }
+    },
+    mounted () {
+      window.addEventListener('scroll', debounce(() => {
+        (window.scrollY >= this.$el.offsetTop) ? this.sticky = true : this.sticky = false
+      }, 25))
+    },
+    beforeDestroy () {
+      window.removeEventListener('scroll', debounce)
+    },
   }
 </script>
 
 <style>
+  :root {
+    --max-width-toc: 200px;
+  }
+
   .table-of-content {
     display: none;
+    position: relative;
     margin: var(--spacing-large) 0;
   }
-  
+
+  .table-of-content--sticky {
+    position: fixed;
+    top: 0;
+    max-width: var(--max-width-toc);
+  }
+
   @media (min-width: 720px) {
     .table-of-content {
       display: block;
