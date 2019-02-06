7f21d09e2e274c456a803b71a37f38494fb2a80c
Frank Warnaar <frank@voorhoede.nl>
2019-02-06 16:03:51 +0100

    fix(async-page): don't include dev dependencies in production build

--- src/client/lib/async-page.js
+++ src/client/lib/async-page.js
@@ -1,7 +1,63 @@
-import dayjs from 'dayjs'
 import path from 'path'
 
 import { SET_I18N_SLUGS } from '~/store/mutation-types'
+
+const fetchPage = process.env.NODE_ENV === 'production'
+  ? function ({ route }) {
+    const filepath = path.join('/data', route.path, 'index.json')
+    if (process.client) {
+      // On client load over http
+      return fetch(filepath).then(res => res.json())
+    } else {
+      // On server load from file system
+      const data = JSON.parse(require('fs').readFileSync(`src/client/static${filepath}`, 'utf8'))
+      return Promise.resolve(data)
+    }
+  }
+  // Fetch page data directly from graphql api in development
+  : function ({ route, i18n }) {
+    // Require modules here, so they don't get included in production build
+    const dayjs = require('dayjs')
+
+    const { doQuery } = require('./query-api')
+    const addClassesToHeadings = require('./add-classes-to-headings')
+    const prismifyCodeBlocks = require('./prismify-code-blocks')
+
+    let queryPath
+
+    const slug = route.params.slug
+    const routeName = route.name.match(`.*(?=${i18n.routesNameSeparator})`)[0]
+    const variables = {
+      locale: i18n.locale,
+      currentDate: dayjs().format('YYYY-MM-DD')
+    }
+
+    if (slug) {
+      const slugRouteName = routeName.split('-slug')[0]
+      queryPath = `${slugRouteName}/_slug`
+      variables.slug = slug
+    } else if (routeName === 'index') {
+      queryPath = `${routeName}`
+    } else {
+      queryPath  = `${routeName}/index`
+    }
+
+    let query
+    if (process.client) {
+      query = require(`../pages/${queryPath}.query.graphql`)
+    } else {
+      query = require('fs').readFileSync(`src/client/pages/${queryPath}.query.graphql`, 'utf8')
+    }
+
+    return doQuery({ query, variables })
+      .then(data => {
+        if (data.page && Array.isArray(data.page.items)) {
+          addClassesToHeadings(data.page.items)
+          prismifyCodeBlocks(data.page.items)
+        }
+        return data
+      })
+  }
 
 export default async function asyncPage ({ route, store, error, app } ) {
   try {
@@ -13,68 +69,4 @@ export default async function asyncPage ({ route, store, error, app } ) {
       statusCode: 404,
     })
   }
-}
-
-export function fetchPage({ route, i18n }) {
-  if (process.env.NODE_ENV !== 'production') {
-    return fetchPageDev({ route, i18n })
-  } else {
-    return fetchPageProduction({ route })
-  }
-}
-
-function fetchPageProduction ({ route }) {
-  const filepath = path.join('/data', route.path, 'index.json')
-  if (process.client) {
-    // On client load over http
-    return fetch(filepath).then(res => res.json())
-  } else {
-    // On server load from file system
-    const data = JSON.parse(require('fs').readFileSync(`src/client/static${filepath}`, 'utf8'))
-    return Promise.resolve(data)
-  }
-}
-
-/*
- * Fetch page data directly from graphql api
- */
-function fetchPageDev ({ route, i18n }) {
-  let queryPath
-
-  const slug = route.params.slug
-  const routeName = route.name.match(`.*(?=${i18n.routesNameSeparator})`)[0]
-  const variables = {
-    locale: i18n.locale,
-    currentDate: dayjs().format('YYYY-MM-DD')
-  }
-
-  if (slug) {
-    const slugRouteName = routeName.split('-slug')[0]
-    queryPath = `${slugRouteName}/_slug`
-    variables.slug = slug
-  } else if (routeName === 'index') {
-    queryPath = `${routeName}`
-  } else {
-    queryPath  = `${routeName}/index`
-  }
-
-  let query
-  if (process.client) {
-    query = require(`../pages/${queryPath}.query.graphql`)
-  } else {
-    query = require('fs').readFileSync(`src/client/pages/${queryPath}.query.graphql`, 'utf8')
-  }
-
-  const { doQuery } = require('./query-api')
-  const addClassesToHeadings = require('./add-classes-to-headings')
-  const prismifyCodeBlocks = require('./prismify-code-blocks')
-
-  return doQuery({ query, variables })
-    .then(data => {
-      if (data.page && Array.isArray(data.page.items)) {
-        addClassesToHeadings(data.page.items)
-        prismifyCodeBlocks(data.page.items)
-      }
-      return data
-    })
 }
