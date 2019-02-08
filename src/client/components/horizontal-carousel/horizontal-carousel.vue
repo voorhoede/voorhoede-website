c85076cf121206ec3308ef33c1701616eea5d9cb
Jesse Eikema <jesse@voorhoede.nl>
2019-02-08 13:25:13 +0100

    refactor(hc): refactor class names update calculation

--- src/client/components/horizontal-carousel/horizontal-carousel.vue
+++ src/client/components/horizontal-carousel/horizontal-carousel.vue
@@ -1,65 +1,66 @@
 <template>
-<div class="horizontal-carousel-container">
   <div class="horizontal-carousel grid">
-    <div class="horizontal-carousel__slides">
-      <slot name="slides"/>
+    <div class="horizontal-carousel__item">
+      <div class="horizontal-carousel__item-slides">
+        <slot name="slides"/>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <style>
   :root {
     --horizontal-carousel-slides-count: 3;
     --horizontal-carousel-slide-width-small: calc(100vw - 2 * (var(--grid-margin)));
-  }
-
-  .horizontal-carousel-container {
-    overflow-x: scroll;
+    --horizontal-carousel-slide-width-large: calc(100vw / var(--horizontal-carousel-slides-count));
+    --horizontal-carousel-width-small: calc(100vw * var(--horizontal-carousel-slides-count) - (var(--grid-margin) * var(--horizontal-carousel-slides-count)));
   }
 
   .horizontal-carousel {
-    width: calc(100vw * var(--horizontal-carousel-slides-count) - (var(--spacing-small) * 4));
+    overflow-x: scroll;
+    -webkit-overflow-scrolling: touch;
   }
 
-  .horizontal-carousel-container::-webkit-scrollbar {
+  .horizontal-carousel::-webkit-scrollbar {
     display: none;
   }
 
-  .horizontal-carousel__slides {
+  .horizontal-carousel__item {
+    width: var(--horizontal-carousel-width-small);
+  }
+
+  .horizontal-carousel__item-slides {
     display: flex;
-    flex-direction: row;
   }
 
-  .horizontal-carousel__slides > * {
+  .horizontal-carousel__item-slides > * {
     width: var(--horizontal-carousel-slide-width-small);
-    margin-bottom: var(--spacing-medium);
   }
 
-  .horizontal-carousel__slides > *:not(:first-child):not(:last-child) {
+  .horizontal-carousel__item-slides > *:not(:first-child):not(:last-child) {
     margin-left: var(--spacing-small);
     margin-right: var(--spacing-small);
   }
 
   @media (min-width: 720px) {
-    .horizontal-carousel-container {
+    .horizontal-carousel {
       overflow-x: unset;
     }
 
-    .horizontal-carousel {
+    .horizontal-carousel__item {
       width: 100%;
     }
 
-    .horizontal-carousel__slides {
+    .horizontal-carousel__item-slides {
       flex-direction: row;
       width: 100%;
     }
 
-    .horizontal-carousel__slides > * {
-      width: calc(100% / var(--horizontal-carousel-slides-count));
+    .horizontal-carousel__item-slides > * {
+      width: var(--horizontal-carousel-slide-width-large);
     }
 
-    .horizontal-carousel__slides > *:not(:first-child):not(:last-child) {
+    .horizontal-carousel__item-slides > *:not(:first-child):not(:last-child) {
       margin-left: var(--spacing-medium);
       margin-right: var(--spacing-medium);
     }
