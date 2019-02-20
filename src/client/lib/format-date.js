8f1eb5b20e1942a67bbc786657633fe72a3099a3
Frank Warnaar <frank@voorhoede.nl>
2019-02-20 13:36:33 +0100

    feat(format-date): use dayjs for formatting dates

---
+++ src/client/lib/format-date.js
@@ -0,0 +1,15 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/nl'
+
+
+/*
+ * Format date as string
+ * @param {String || Object} date
+ * @param {String} locale
+ * @param {String} format	to render input in (eg. `DD MMM` or `LL`). See dayjs docs for options: https://github.com/iamkun/dayjs/blob/dev/docs/en/API-reference.md#format-formatstringwithtokens-string
+ */
+export default function ({ date, locale, format }) {
+  return dayjs(date)
+    .locale(locale)
+    .format(format)
+}
