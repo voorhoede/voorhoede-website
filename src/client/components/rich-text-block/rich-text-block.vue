9f779df0eae8febe4a3a71515686d087afe0bceb
Frank Warnaar <frank@voorhoede.nl>
2019-02-20 10:51:28 +0100

    style: code linting issues resolved

--- src/client/components/rich-text-block/rich-text-block.vue
+++ src/client/components/rich-text-block/rich-text-block.vue
@@ -81,8 +81,8 @@
     background-color: var(--paper);
     color: var(--dark-blue);
   }
+
   .rich-text a > code {
-    background-color: transparent;
     color: inherit;
     background: transparent linear-gradient(to top, transparent 2px, var(--paper) 1px);
   }
