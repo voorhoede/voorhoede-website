b348d935c5425991d44a7fc6c82460ede66ae02e
Frank Warnaar <frank@voorhoede.nl>
2019-03-20 15:55:33 +0100

    feat(event-list-item): new component

---
+++ src/client/components/event-list-item/event-list-item.vue
@@ -0,0 +1,101 @@
+<template>
+  <article class="event-list-item">
+    <nuxt-link
+      class="event-list-item__link"
+      :to="localeUrl({ name: 'events-slug', params: { slug: event.slug } })"
+    >
+      <div class="event-list-item__transition-box">
+        <calendar-icon :date="event.date" fill="dim"/>
+        <div class="event-list-item__content">
+          <h3 class="event-list-item__title event-list-item__title--compact body">{{ event.title }}</h3>
+          <h3 class="event-list-item__title event-list-item__title--large h4">{{ event.title }}</h3>
+          <rich-text-block v-if="event.description" :text="event.description"/>
+        </div>
+      </div>
+    </nuxt-link>
+  </article>
+</template>
+
+<script>
+  import CalendarIcon from '../calendar-icon'
+  import RichTextBlock from '../rich-text-block'
+
+  export default {
+    components: {
+      CalendarIcon,
+      RichTextBlock,
+    },
+    props: {
+      event: {
+        type: Object,
+        required: true,
+        validator(event) {
+          return (
+            typeof(event.slug) === 'string'
+            && typeof(event.date) === 'string'
+            && typeof(event.title) === 'string'
+            && typeof(event.description) === 'string'
+          )
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  :root {
+    --event-list-item-transition: .3s cubic-bezier(.47, 0, .29, .98);
+  }
+
+  .event-list-item__transition-box {
+    padding: var(--spacing-small) 0;
+    transition: transform var(--event-list-item-transition);
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+  }
+
+  .event-list-item__link:hover .event-list-item__transition-box,
+  .event-list-item__link:focus .event-list-item__transition-box {
+    transform: translateX(var(--grid-margin));
+  }
+
+  .event-list-item .calendar-icon {
+    flex-shrink: 0;
+  }
+
+  .event-list-item__content {
+    margin-left: var(--spacing-large);
+  }
+
+  .event-list-item__title--large {
+    display: none;
+  }
+
+  .event-list-item .rich-text {
+    display: none;
+  }
+
+  .page-event__upcoming-events-list {
+    margin-top: var(--spacing-larger);
+    margin-bottom: var(--spacing-large);
+  }
+
+  @media (min-width: 720px) {
+    .event-list-item__transition-box {
+      align-items: flex-start;
+    }
+
+    .event-list-item__title--compact {
+      display: none;
+    }
+
+    .event-list-item__title--large {
+      display: block;
+    }
+
+    .event-list-item .rich-text {
+      display: block;
+    }
+  }
+</style>
