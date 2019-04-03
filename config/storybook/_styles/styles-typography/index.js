c38f02d27f2672e066ae054e869f20d385440ea3
Declan Rek <declanrek@gmail.com>
2019-04-03 16:23:02 +0200

    refactor: remove storybook

--- config/storybook/_styles/styles-typography/index.js
+++
@@ -1,5 +0,0 @@
-import fallbackTypography from './fallback-typography.vue'
-import Component from './styles-typography.vue'
-
-export default Component
-export const FallbackTypography = fallbackTypography
