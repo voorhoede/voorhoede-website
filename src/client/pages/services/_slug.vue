24bce40e694fbdfcdc317ddb6537b4d2dc62abfb
Jesse Eikema <jesse@voorhoede.nl>
2019-06-20 16:21:19 +0200

    feat(services): add fixed ratio and image component to service detail page and updated query

--- src/client/pages/services/_slug.vue
+++ src/client/pages/services/_slug.vue
@@ -9,10 +9,20 @@
     <article class="page-service__overview">
       <template v-for="item in page.items">
         <generic-text-block
+          v-if="item.__typename === 'GenericTextBlockRecord'"
           :key="item.title"
           :title="item.title"
           :body="item.body"
           :image="item.image" />
+        <fixed-ratio
+          :height="item.image.height"
+          :width="item.image.width"
+          :key="item.image.url"
+          v-if="item.__typename === 'ImageRecord'">
+          <app-image
+            :image="item.image"
+          />
+        </fixed-ratio>
       </template>
     </article>
     <pivot-section
@@ -29,12 +39,16 @@
   import PageHeader from '~/components/page-header'
   import GenericTextBlock from '~/components/generic-text-block'
   import PivotSection from '~/components/pivot-section'
+  import AppImage from '~/components/app-image'
+  import FixedRatio from '~/components/fixed-ratio'
 
   export default {
     components: {
       PageHeader,
       GenericTextBlock,
       PivotSection,
+      FixedRatio,
+      AppImage
     },
     asyncData,
     head,
@@ -47,12 +61,20 @@
   }
 
   .page-service__overview {
+    display: flex;
+    flex-direction: column;
     grid-row: 2;
   }
 
+  .page-service__overview .fixed-ratio,
   .page-service__overview .generic-text-block {
     grid-row: 3;
     margin-bottom: var(--spacing-large);
+  }
+
+  .page-service__overview .image-container {
+    width: 100%;
+    height: 100%;
   }
 
   .page-service__pivot-section.pivot-section {
@@ -73,6 +95,10 @@
       grid-column: var(--grid-content);
       background-color: var(--white);
       padding: var(--spacing-large) var(--spacing-larger);
+    }
+
+    .page-service__overview .fixed-ratio {
+      width: 63%;
     }
   }
 
