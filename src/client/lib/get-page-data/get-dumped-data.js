84cca69c5985a052ef4c21155ddafe88b0f0298e
Frank Warnaar <frank@voorhoede.nl>
2019-07-17 13:42:28 +0200

    feat(get-dumped-data): use dynamic imports to get data hashed

--- src/client/lib/get-page-data/get-dumped-data.js
+++ src/client/lib/get-page-data/get-dumped-data.js
@@ -1,14 +1,14 @@
 import path from 'path'
 
 export default function ({ route }) {
-  const filepath = path.join('/data', route.path, 'index.json')
+  const filePath = path.join(route.path, 'index.json')
 
   if (process.client) {
     // On client load over http
-    return fetch(filepath).then(res => res.json())
+    return import(/* webpackChunkName: "pageData" */ `../../static/data${filePath}`).then(module => module.default)
   } else {
     // On server load from file system
-    const data = JSON.parse(require('fs').readFileSync(`src/client/static${filepath}`, 'utf8'))
+    const data = JSON.parse(require('fs').readFileSync(`src/client/static/data${filePath}`, 'utf8'))
     return Promise.resolve(data)
   }
 }
