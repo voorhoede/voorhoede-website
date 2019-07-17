e53c04c2f7f25943fcaf1a6c5ce6a89e14dd08e2
Frank Warnaar <frank@voorhoede.nl>
2019-07-17 14:48:35 +0200

    docs(get-page-data): chunk name added

--- src/client/lib/get-page-data/index.js
+++ src/client/lib/get-page-data/index.js
@@ -4,7 +4,7 @@ export default async function (context) {
   if (process.env.useDumpedData) {
     return getDumpedData(context)
   } else {
-    const queryDato = await import('./query-dato').then(module => module.default)
+    const queryDato = await import(/* webpackChunkName: "dev-dato-query" */ './query-dato').then(module => module.default)
     return queryDato(context)
   }
 }
