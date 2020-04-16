c30ff0ac764040feb875bca1a3d7e74c90c50c59
Vincent van Dijck <vincent@voorhoede.nl>
2020-04-16 10:24:57 +0200

    feat(pages) correct updates pages

---
+++ src/client/pages/updates/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <main class="page-blog">
+    <page-header
+      heading="byline"
+      :byline="page.title"
+      :headline="page.subtitle"
+      :image="page.headerIllustration"
+    />
+    <section class="page-blog-container grid">
+      <h2 class="sr-only">{{ $t('blog_overview') }}</h2>
+      <text-block class="page-blog__text">
+        <p class="testimonial">{{ page.description }}</p>
+      </text-block>
+      <ul class="page-blog__posts">
+        <li v-for="blogPost in items.filter(post => post.published)" :key="blogPost.slug">
+          <blog-list-item large :item="blogPost" />
+        </li>
+      </ul>
+    </section>
+    <div class="page-blog__cta grid">
+      <!-- TODO: Make this a section to be defined in DATO (once for all blog pages) -->
+      <pivot-section
+        v-if="pivots && pivots.length"
+        class="page-blog__pivot-section"
+        :pivot="pivots[0]"
+      />
+      <scroll-to direction="up" />
+    </div>
+  </main>
+</template>
+
+<script>
+  import asyncData from '~/lib/async-page'
+  import head from '~/lib/seo-head'
+
+  import BlogListItem from '~/components/blog-list-item'
+  import PivotSection from '~/components/pivot-section'
+  import PageHeader from '~/components/page-header'
+  import ScrollTo from '~/components/scroll-to'
+  import TextBlock from '~/components/text-block'
+
+  export default {
+    components: {
+      BlogListItem,
+      PivotSection,
+      PageHeader,
+      ScrollTo,
+      TextBlock
+    },
+    asyncData,
+    head
+  }
+</script>
+
+<style>
+  .page-blog-container {
+    padding-top: var(--spacing-larger);
+  }
+
+  .page-blog__text {
+    margin-bottom: var(--spacing-larger);
+    grid-row: 1;
+    color: var(--html-blue);
+  }
+
+  .page-blog__posts {
+    grid-row: 2;
+  }
+
+  .page-blog__cta {
+    grid-row: 3;
+    grid-column: var(--grid-page);
+    background-color: var(--bg-pastel);
+    position: relative;
+  }
+
+  .page-blog__cta .pivot-section {
+    border: none;
+  }
+
+  .page-blog__cta .scroll-to {
+    display: none;
+  }
+
+  @media (min-width: 720px) {
+    .page-blog__text,
+    .page-blog__posts {
+      grid-column-start: 5;
+      grid-column-end: 47;
+      margin-bottom: var(--spacing-large);
+    }
+
+    .page-blog-container {
+      position: relative;
+    }
+
+    .page-blog__cta .scroll-to {
+      display: flex;
+      position: absolute;
+      bottom: var(--spacing-larger);
+      grid-column: 48;
+    }
+
+    .page-blog__pivot-section {
+      grid-column-start: 8;
+      grid-column-end: 46;
+    }
+  }
+
+  @media (min-width: 1100px) {
+    .page-blog__text,
+    .page-blog__posts {
+      grid-column-start: 14;
+      grid-column-end: 42;
+      margin-bottom: var(--spacing-large);
+    }
+
+    .page-blog__pivot-section {
+      grid-column-start: 14;
+      grid-column-end: 38;
+    }
+
+    .page-blog__cta .scroll-to {
+      bottom: var(--spacing-big);
+    }
+  }
+</style>
