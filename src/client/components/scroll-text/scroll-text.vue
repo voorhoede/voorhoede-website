a64137fb46b7350e56c29d913201c2c16b510f88
Benedikt Deitmer <you@hornebom.com>
2020-07-15 14:22:45 +0200

    first approach

--- src/client/components/scroll-text/scroll-text.vue
+++ src/client/components/scroll-text/scroll-text.vue
@@ -1,35 +1,30 @@
 <template>
-  <div
-    class="scroll-text"
-    ref="container"
-  >
-    <slot/>
+  <div class="scroll-text">
+    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">Wij helpen je vooruit op het web</div>
+    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">Beginnen klein en itereren snel</div>
+    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">Om voor onze eindgebruikers,</div>
+    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">geweldige producten te ontwikkelen</div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    isIntersectionDisabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
   data () {
     return {
-      isIntersected: this.isIntersectionDisabled,
+      isIntersected: false,
       observer: null,
-      isAnimated: this.isIntersectionDisabled,
       rootMargin: '0px 0px 0px 0px',
-      threshold: [.1, .2, .3, .4, .5]
+      threshold: [0, .25, .5, .75, 1],
+      isAnimating: false,
+      items: [],
+      centerScreen: 0,
+      containerTop: 0
     }
   },
   mounted() {
-    if ('IntersectionObserver' in window && !this.isIntersectionDisabled) {
+    if ('IntersectionObserver' in window) {
       this.observe()
-      this.isAnimated = true
-    } else {
-      this.isIntersected = true
+      this.items = [...this.$el.querySelectorAll('[scroll-text-item]')]
     }
   },
   beforeDestroy() {
@@ -43,21 +38,68 @@ export default {
       this.observer.observe(this.$el)
     },
     onIntersection(entries) {
-      if(entries[0].isIntersecting) console.log('entries', entries[0].intersectionRatio)
       this.isIntersected = entries.some((entry) => entry.intersectionRatio > 0)
-      if (this.isIntersected) {
-        // this.unobserve()
+      if (this.isIntersected && !this.isAnimating) {
+        this.isAnimating = window.requestAnimationFrame(this.animate)
+      }
+      if (!this.isIntersected) {
+        window.cancelAnimationFrame(this.isAnimating)
+        this.isAnimating = false
       }
     },
     unobserve() {
-      if ('IntersectionObserver' in window && !this.isIntersectionDisabled) {
+      if ('IntersectionObserver' in window) {
         this.observer.unobserve(this.$el)
       }
+    },
+    animate() {
+      this.setScaleVector()
+      if(this.isAnimating) {
+        this.isAnimating = window.requestAnimationFrame(this.animate)
+      }
+    },
+    setScaleVector() {
+      this.centerScreen = window.innerHeight * .5
+      this.containerTop = this.$el.getBoundingClientRect().top
+      let item, top, height, centerItem, distance, zPosition
+
+      for(let i = 0; i < this.items.length; i++) {
+        item = this.items[i]
+        top = this.containerTop + item.offsetTop
+        height = item.clientHeight
+
+        centerItem = top + height * .5
+        distance = this.distanceTo(this.centerScreen, centerItem)
+
+        zPosition = -1 + this.smoothstep(this.centerScreen, 0, distance)
+        item.style.setProperty('--z-position', `${zPosition}px`)
+      }
+    },
+    distanceTo(x1, x2) {
+      let x = x2 - x1
+      x *= x
+      return Math.sqrt(x)
+    },
+    smoothstep (min, max, value) {
+      const x = Math.max(0, Math.min(1, (value - min) / (max - min)))
+      return x * x * (3 - 2 * x)
     },
   },
 }
 </script>
 
 <style>
+  .scroll-text {
+    padding-bottom: 5vh;
+    transform-style: preserve-3d;
+    perspective: 3;
+  }
 
+  .scroll-text__item {
+    text-align: center;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    transform: translateZ(var(--z-position));
+    transition: transform .1s linear;
+  }
 </style>
