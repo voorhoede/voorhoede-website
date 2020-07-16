675cd53433afd28feda9fddf5a6a6862cd324ec0
Benedikt Deitmer <you@hornebom.com>
2020-07-16 07:54:04 +0200

    removes scroll-highlighted-text ++ creates directive for scroll-text js ++ add scroll-text component

--- src/client/components/scroll-text/scroll-text.vue
+++ src/client/components/scroll-text/scroll-text.vue
@@ -1,105 +1,68 @@
 <template>
-  <div class="scroll-text">
-    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">Wij helpen je vooruit op het web</div>
-    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">Beginnen klein en itereren snel</div>
-    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">Om voor onze eindgebruikers,</div>
-    <div class="scroll-text__item h1" scroll-text-item style="--z-position: -1px;">geweldige producten te ontwikkelen</div>
-  </div>
+  <p class="scroll-text" v-scroll-highlight>
+    <span
+      v-for="item in items"
+      :key="item.line"
+      class="scroll-text__item h1"
+      scroll-highlight-item
+    >
+      {{ item.line }}
+    </span>
+  </p>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      isIntersected: false,
-      observer: null,
-      rootMargin: '0px 0px 0px 0px',
-      threshold: [0, .25, .5, .75, 1],
-      isAnimating: false,
-      items: [],
-      centerScreen: 0,
-      containerTop: 0
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      validator(value) {
+        return value.every(item => item)
+      }
     }
-  },
-  mounted() {
-    if ('IntersectionObserver' in window) {
-      this.observe()
-      this.items = [...this.$el.querySelectorAll('[scroll-text-item]')]
-    }
-  },
-  beforeDestroy() {
-    this.unobserve()
-  },
-  methods: {
-    observe() {
-      const { rootMargin, threshold } = this
-      const config = { root: undefined, rootMargin, threshold }
-      this.observer = new IntersectionObserver(this.onIntersection, config)
-      this.observer.observe(this.$el)
-    },
-    onIntersection(entries) {
-      this.isIntersected = entries.some((entry) => entry.intersectionRatio > 0)
-      if (this.isIntersected && !this.isAnimating) {
-        this.isAnimating = window.requestAnimationFrame(this.animate)
-      }
-      if (!this.isIntersected) {
-        window.cancelAnimationFrame(this.isAnimating)
-        this.isAnimating = false
-      }
-    },
-    unobserve() {
-      if ('IntersectionObserver' in window) {
-        this.observer.unobserve(this.$el)
-      }
-    },
-    animate() {
-      this.setScaleVector()
-      if(this.isAnimating) {
-        this.isAnimating = window.requestAnimationFrame(this.animate)
-      }
-    },
-    setScaleVector() {
-      this.centerScreen = window.innerHeight * .5
-      this.containerTop = this.$el.getBoundingClientRect().top
-      let item, top, height, centerItem, distance, zPosition
-
-      for(let i = 0; i < this.items.length; i++) {
-        item = this.items[i]
-        top = this.containerTop + item.offsetTop
-        height = item.clientHeight
-
-        centerItem = top + height * .5
-        distance = this.distanceTo(this.centerScreen, centerItem)
-
-        zPosition = -1 + this.smoothstep(this.centerScreen, 0, distance)
-        item.style.setProperty('--z-position', `${zPosition}px`)
-      }
-    },
-    distanceTo(x1, x2) {
-      let x = x2 - x1
-      x *= x
-      return Math.sqrt(x)
-    },
-    smoothstep (min, max, value) {
-      const x = Math.max(0, Math.min(1, (value - min) / (max - min)))
-      return x * x * (3 - 2 * x)
-    },
-  },
+  }
 }
+
 </script>
 
 <style>
   .scroll-text {
-    padding-bottom: 5vh;
+    position: relative;
+    padding-bottom: var(--spacing-medium);
+  }
+
+  .scroll-text.scroll-text--active {
     transform-style: preserve-3d;
-    perspective: 3;
+    perspective: 3px;
   }
 
   .scroll-text__item {
+    display: block;
+    margin-bottom: var(--spacing-medium);
     text-align: center;
-    padding-top: 50px;
-    padding-bottom: 50px;
+  }
+
+  .scroll-text--active .scroll-text__item {
     transform: translateZ(var(--z-position));
     transition: transform .1s linear;
+  }
+
+  @media (max-width: 520px) {
+    .scroll-text__item {
+      font-size: 32px;
+    }
+  }
+
+  @media (min-width: 720px) {
+    .scroll-text__item {
+      margin-bottom: var(--spacing-large);
+    }
+  }
+
+  @media (min-width: 1100px) {
+    .scroll-text__item {
+      margin-bottom: var(--spacing-larger);
+    }
   }
 </style>
