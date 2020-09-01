158b49e000a9f08769d666eacdb9dcddb945aa1c
Celine <celinekurpershoek@gmail.com>
2020-09-01 10:51:00 +0200

    feat(sitemap): add priorities

--- config/nuxt/modules/sitemap.js
+++ config/nuxt/modules/sitemap.js
@@ -3,22 +3,47 @@ At v2.4.0 @nuxtjs/sitemap module can't generate i18n supported dynamic routes.
 Thats why sitemaps are generated based on locales.
 */
 const locales = require('../../../src/client/static/data/app.json').locales
-const routes = require('../lib/routes');
+const routes = require('../lib/routes')
+
+function getPriority (route) {
+    let value = 0.5
+    const priorties = {
+        services: 0.9,
+        cases: 0.8,
+        blog: 0.7,
+        events: 0.6
+    }
+    for(let key in priorties) {
+       if (route.includes(key.toString())) {
+            value = priorties[key]
+        }
+     }
+    return value
+}
 
 module.exports = [
     '@nuxtjs/sitemap',
     {
         hostname: process.env.URL,
-        i18n: {
-            locales: locales.map(locale => locale.code)
-        },
+        exclude: [
+            '*/*',
+        ],
         sitemaps: locales.map(locale => {
-          const regex = new RegExp(locale.code + '/')
+          const regex = new RegExp('^'+ locale.code + '/')
           return {
             path: `/sitemap-${locale.code}.xml`,
             lang: locale.code,
-            routes: routes.filter(route => route.match(regex, "g"))
-          }
+            exclude: ['/**'],
+            routes: routes
+                .filter(route => route.match(regex, 'g'))
+                .map(route => {
+                    return {
+                        url: route,
+                        priority: getPriority(route),
+                        gzip: true
+                    }
+                })
+            }
         })
     }
 ]
