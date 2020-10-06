050753a2a5d6e3f0b412c57e7226fc75f9d14865
Stefan Kool <stefankool78@gmail.com>
2020-10-06 11:36:43 +0200

    tidy up gtm plugin file

--- src/client/plugins/analytics-gtm.js
+++ src/client/plugins/analytics-gtm.js
@@ -6,26 +6,18 @@ const UA_VERSION = datoConstants.trackingVersion
 const TRACKING_TRANSPORT = 'gtm.js'
 
 export default () => {
-    // 1. create dataLayer
-    window.dataLayer = window.dataLayer || []
-    // 2. push object to start gtm and populate custom dimensions
-    window.dataLayer.push({
-      'gtm.start': new Date().getTime(),
-      event: 'gtm.js',
-      'trackingVersion': UA_VERSION,
-      'trackingTransport': TRACKING_TRANSPORT,
-      'environmentName': ENVIRONMENT_NAME,
-    })
-    // 3. add gtm script to page
-    const script = document.createElement('script')
-    script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`
-    document.head.appendChild(script)
-
-  initTagManager()
+  // 1. create dataLayer
+  window.dataLayer = window.dataLayer || []
+  // 2. push object to start gtm and populate custom dimensions
+  window.dataLayer.push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js',
+    'trackingVersion': UA_VERSION,
+    'trackingTransport': TRACKING_TRANSPORT,
+    'environmentName': ENVIRONMENT_NAME,
+  })
+  // 3. add gtm script to page
+  const script = document.createElement('script')
+  script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`
+  document.head.appendChild(script)
 }
-
-function initTagManager() {
-
-}
-
-
