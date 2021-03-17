6a88e08101237ee8999c5f2a7f3bf274f7417f26
StefanKool <stefankool78@gmail.com>
2021-03-17 17:13:19 +0100

    PR comment contact form

--- src/client/components/contact-form/contact-form.vue
+++ src/client/components/contact-form/contact-form.vue
@@ -180,11 +180,8 @@
         })
       },
       trackEvent () {
-        if(this.formIsValidated === true ) {
-          return this.$ga.event('Contact form', 'click submit', 'success', 0)
-        } else {
-          return this.$ga.event('Contact form', 'click submit', 'failed', 0)
-        }
+        const formValidationState = this.formIsValidated ? 'success' : 'failed'
+        return this.$ga.event('Newsletter form', 'click submit', formValidationState, 0)
       },
     }
   }
