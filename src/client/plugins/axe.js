5a6239b6f6aba25e8385c6fc527d431613bef5ec
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2021-05-07 10:32:31 +0200

    Configure Axe using best practices and the latest rules of the WCAG

--- src/client/plugins/axe.js
+++ src/client/plugins/axe.js
@@ -4,5 +4,12 @@ if (process.env.NODE_ENV === 'development') {
   const VueAxe = require('vue-axe').default
   Vue.use(VueAxe, {
     allowConsoleClears: false,
+    runOptions: {
+      reporter: 'v2',
+      runOnly: {
+        type: 'tag',
+        values: ['best-practice', 'wcag21a', 'wcag21aa'],
+      },
+    },
   })
 }
