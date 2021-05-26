27a31325e13595f4e79c1daa94b7462af4ec8a40
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2021-05-26 15:54:50 +0200

    Remove error for testing

--- src/client/components/curly-bracket/curly-bracket.vue
+++ src/client/components/curly-bracket/curly-bracket.vue
@@ -1,6 +1,7 @@
 <template>
   <div :class="`curly-bracket curly-bracket--${side}`">
     <img
+      alt=""
       class="curly-bracket__image"
       :src="`/images/curly-bracket--${color}.svg`"
     >
