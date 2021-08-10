bd347d001ae3aa5430b9e6a942542c0d1ad07a85
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-08-10 10:34:57 +0200

    Added dummy content

---
+++ src/client/components/contact-page-form/contact-page-form.vue
@@ -0,0 +1,274 @@
+<template>
+  <div
+    class="contact-form"
+    :class="{ 'grid': hasSidebar }"
+  >
+    <div v-if="hasSidebar" class="contact-form__header">
+      <h2
+        v-if="title"
+        class="contact-form__header h3"
+      >
+        {{ title }}
+      </h2>
+      <div v-if="contactPerson" class="contact-form__contact-person">
+        <responsive-image :image="contactPerson.image"/>
+        <p class="h5">{{ contactPerson.name }} {{ contactPerson.lastName }}</p>
+        <p
+          v-if="contactPerson.jobTitle"
+          class="body-petite contact-page-form__input-field"
+        >
+          {{ contactPerson.jobTitle }}
+        </p>
+        <a
+          class="body-petite contact-page-form__input-field link"
+          href="mailto:post@voorhoede.nl"
+        >
+          post@voorhoede.nl
+        </a>
+      </div>
+    </div>
+    <form
+      @submit.prevent="submit"
+      method="POST"
+      :name="form['form-name']"
+      :action="localeUrl({ name: 'contact-slug', params: { slug: 'confirmation' } })"
+      class="contact-form__form"
+      data-netlify="true"
+      netlify-honeypot="magic-castle"
+      :novalidate="useCustomValidation"
+    >
+      <fieldset>
+        <legend class="sr-only">{{ ariaLabelOrTitle }}</legend>
+        <div class="contact-page-form__personal-details">
+          <div class="contact-page-form__input">
+            <input type="hidden" name="form-name" :value="form['form-name']">
+            <input type="text" name="subject" :value="form.name" class="hidden"/>
+            <label class="hidden">
+              Don't fill this out if you're human:
+              <input v-model="form.magicCastle" name="magic-castle">
+            </label>
+            <input-field
+              v-model="form.name"
+              id="name"
+              type="text"
+              @input="createEmailSubject"
+              :label="$t('my_name_is')"
+              :placeholder-label="$t('your_name')"
+              required
+              :validate="formIsValidated"
+              :validation-error-message="$t('name_is_required')"
+              class="body-petite contact-page-form__input-field"
+            />
+            <input-field
+              v-model="form.business"
+              id="business"
+              type="text"
+              :label="$t('my_business_is')"
+              :placeholder-label="$t('company_name')"
+              class="body-petite contact-page-form__input-field"
+            />
+          </div>
+          
+          <div class="contact-page-form__input">
+            <input-field
+              v-model="form.email"
+              id="email"
+              type="email"
+              :label="$t('you_can_email_me_at')"
+              :placeholder-label="$t('email_address')"
+              required
+              :validate="formIsValidated"
+              :validation-error-message="emailValidationErrorMessage"
+              class="body-petite contact-page-form__input-field"
+            />
+            <input-field
+              v-model="form.phone"
+              id="phone"
+              type="tel"
+              :label="$t('you_can_call_me_at')"
+              :placeholder-label="$t('phone_number')"
+              class="body-petite contact-page-form__input-field"
+            />
+          </div>
+        </div>
+
+        <input-field
+          textarea
+          v-model="form.explanation"
+          id="explanation"
+          type="text"
+          :label="$t('my_project_is')"
+          :placeholder-label="$t('project_description')"
+          class="body-petite contact-page-form__input-field"
+        />
+        <app-button
+          @click.native="trackEvent()"
+          class="contact-form__button"
+          :label="$t('get_in_touch')"
+          type="submit"
+        />
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+  import submitContactForm from '../../lib/submit-contact-form'
+
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: false,
+        default: undefined
+      },
+      ariaLabel: {
+        type: String,
+        required: false,
+        default () {
+          this.$t('lets_discuss')
+        },
+      },
+      contactPerson: {
+        type: Object,
+        default: undefined,
+        validator(contactPerson) {
+          return !contactPerson || (
+            typeof(contactPerson.name) === 'string'
+            && typeof(contactPerson.lastName) === 'string'
+            && typeof(contactPerson.image) === 'object'
+          )
+        }
+      }
+    },
+    data() {
+      return {
+        form: {
+          'form-name': 'get-in-touch',
+          name: '',
+          email: '',
+          phone: '',
+          business: '',
+          explanation: '',
+          subject: '',
+        },
+        formIsValidated: false,
+        useCustomValidation: false,
+      }
+    },
+    computed: {
+      ariaLabelOrTitle () {
+        return this.ariaLabel || this.title
+      },
+      emailValidationErrorMessage() {
+        return this.form.email ? this.$t('provide_valid_email') : this.$t('email_is_required')
+      },
+      hasSidebar() {
+        return this.title || this.contactPerson
+      }
+    },
+    mounted() {
+      this.useCustomValidation = true
+    },
+    methods: {
+      createEmailSubject(name) {
+        this.form.subject = `${name} has sent a message`
+      },
+      submit(event) {
+        this.formIsValidated = true
+        if (!event.target.checkValidity()) {
+          return false
+        }
+        submitContactForm({
+          form: this.form,
+          router: this.$router,
+          localeUrl: this.localeUrl,
+        })
+      },
+      trackEvent () {
+        const formValidationState = this.formIsValidated ? 'success' : 'failed'
+        return this.$ga.event('Newsletter form', 'click submit', formValidationState, 0)
+      },
+    }
+  }
+</script>
+
+<style>
+  @import '../forms/forms.css';
+
+  :root {
+    --contact-form-thumbnail-size: 120px;
+  }
+
+  .contact-form__header {
+    grid-row: 1;
+  }
+
+  .contact-form__contact-person {
+    display: none;
+  }
+
+  .contact-form__contact-person .responsive-image {
+    margin-bottom: var(--spacing-small);
+    width: var(--contact-form-thumbnail-size-small);
+  }
+
+  .contact-form__header ~ .contact-form__form {
+    grid-row: 2;
+  }
+
+  .contact-form__label:first-of-type {
+    margin-top: 0;
+  }
+
+  .contact-form__button {
+    margin-top: var(--spacing-larger);
+  }
+
+  @media (min-width: 520px) {
+    .contact-form__contact-person .responsive-image {
+      margin-left: 0;
+      margin-right: 0;
+    }
+
+    .contact-page-form__personal-details {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .contact-page-form__input {
+      width: 40%;
+    }
+    
+    .contact-page-form__input-field {
+      flex-direction: column;
+    }
+  }
+
+  @media (min-width: 1100px) {
+    .contact-form__header {
+      grid-column-start: 6;
+      grid-column-end: 18;
+      margin-bottom: var(--spacing-medium);
+    }
+
+    .contact-form__contact-person {
+      display: block;
+    }
+
+    .contact-form__contact-person .responsive-image {
+      width: var(--contact-form-thumbnail-size);
+    }
+
+    .contact-form__header ~ .contact-form__form {
+      grid-column-start: 21;
+      grid-column-end: 46;
+      grid-row: 1;
+    }
+
+    .contact-form__header ~ .contact-form__form > .contact-form__label-text {
+      width: 9rem;
+    }
+  }
+</style>
--- src/client/pages/contact/index.vue
+++ src/client/pages/contact/index.vue
@@ -1,26 +1,72 @@
 <template>
-  <main>
-    <section class="page-contact">
-      <page-header
+  <main class="page-contact grid">
+    <section>
+      <!-- <page-header
         heading="byline"
         :byline="page.title"
         :headline="page.subtitle"
         :image="page.headerIllustration"
+      /> -->
+      <page-header
+        heading="byline"
+        :byline="dummyContent.subtitle"
+        :headline="dummyContent.title"
+        :image="page.headerIllustration"
       />
+      
+      <div class="usps">
+        <!-- <h2 class="h3 page-work-at__layout">{{ dummyContent.subtitle }}</h2> -->
+        <div class="usps__items page-contact__usps-items">
+          <div class="usps__item page-contact__usps-item" v-for="usp in usps" :key="usp.title">
+            <section class="page-contact__usps-item-body rich-text">
+              <img :src="usp.image.url" class="usps__item-image page-contact__usps-item-image" alt="">
+              <h3 class="h4 subtitle page-contact__usps-title">{{ usp.title }}</h3>
+              <div v-html="usp.body" class="body h2 font-bold" />
+            </section>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="page-contact__form-container rich-text grid">
+      <div class="page-contact__form-header">
+        <h2 class="h2 page-contact__body-title">{{ form.title }}</h2>
+        <p class="sub-title page-contact__body-subtitle">{{ form.subtitle }} </p>
+      </div>
+      <!-- <img src="https://i.imgur.com/2GjGyOX.png" alt="" class="usps__item-image page-contact__usps-item-image"> -->
       <div class="page-contact__backdrop grid">
-        <div class="page-contact__content">
-          <contact-sidebar
+   
+        <!-- <contact-sidebar
             :email-label="page.emailLabel"
             :phone-label="page.phoneLabel"
             :address-label="page.addressLabel"
             :addresses="app.addresses"
-          />
-          <contact-form
-            :aria-label="$t('lets_discuss')"
-          />
-        </div>
+          /> -->
+    
+        <contact-page-form
+          class="page-contact__form page-contact__content"
+          :aria-label="$t('lets_discuss')"
+        />
       </div>
+
     </section>
+    
+    <section class="rich-text page-contact__visit">
+      <h2 class="h2">{{ visit.title }}</h2>
+      <p class="body">{{ visit.body }}</p>
+      <ul>
+        <li v-for="location in visit.location" :key="location.title">
+          <article class="case-excerpt">
+            <picture class="case-excerpt__media"><img :src="location.image" class="case-excerpt__image" alt=""></picture>
+            <div class="case-excerpt__caption">
+              <a href="" class="case-except__link"><h3 class="h4 case-excerpt__title">{{ location.title }} </h3></a>
+              <p class="case-excerpt__body body">{{ location.address }}</p>
+            </div>
+          </article>  
+        </li>
+      </ul>
+    </section>
+    
     <newsletter-form :has-background="true" />
   </main>
 </template>
@@ -28,8 +74,59 @@
 <script>
   import asyncData from '~/lib/async-page'
   import head from '~/lib/seo-head'
+
   export default {
     asyncData,
+    data(){
+      return {
+        dummyContent: {
+          title: 'Tell us all about your awesome project!',
+          subtitle: 'Are you excited to start your project but dont’t know where to begin? We are all ears, let’s build it together!',
+          introTitle: 'call us',
+          introBody: '0112343203',
+          image: 'https://i.imgur.com/Ccgdl26.png'
+        },
+        form: {
+          title: 'Say Hello!',
+          subtitle: 'We would like to hear something from you!'
+        },
+        visit: {
+          title: 'Coffee?',
+          body: 'We too love coffee! Head over to one of our location!',
+          location: [{
+            title: 'Amsterdam',
+            address: 'Rijnsburgstraat 9-11 1059 AT Amsterdam',
+            image: 'https://imgur.com/pfUlN9C.png'
+            },
+            {
+            title: 'Delft',
+            address: 'Koornmarkt 22 2611 EG Delft',
+            image: 'https://imgur.com/dRWZrhX.png'
+          }]
+        },
+        usps: [{
+          title: 'Give us a call at',
+          body: '<a href="tel:+31 (0)20 2610954" class="body"> <h3 class="h3"> +31 (0)20 2610954 </h3> </a>',
+          image: {
+            url:'https://i.imgur.com/OdQfz5J.png',
+            alt:'icon-one',
+            width: '40px',
+            height: '40px',
+            format: ''
+            },
+        },{
+          title: '...or mail us',
+          body: '<a href="mailto:post@voorhoede.nl" class="body"> <h3 class="h3"> post@voorhoede.nl </h3> </a>',
+          image: {
+            url:'https://imgur.com/zgRP4L5.png',
+            alt:'icon-two',
+            width: '40px',
+            height: '40px',
+            format: ''
+            },
+        }],
+      }
+    },
     mounted() {
       this.$announcer.set(`${this.$t('page')}: ${this.page.social.title}`, 'polite')
     },
@@ -44,13 +141,21 @@
 
   .page-contact__content {
     padding-top: var(--spacing-large);
-    display: flex;
-    flex-direction: column-reverse;
-    background-color: var(--white);
   }
 
-  .page-contact__backdrop {
+  .page-contact .page-contact__backdrop {
     background-color: var(--white);
+    position: relative;
+  }
+  
+  .page-contact .page-contact__backdrop::after {
+    content: "";
+    position: absolute;
+    height: 100%;
+    width: 2rem;
+    top: 0;
+    right: -2rem;
+    background: var(--white);
   }
 
   .page-contact .contact-sidebar {
@@ -66,15 +171,20 @@
   }
 
   @media screen and (min-width: 720px) {
-    .page-contact__backdrop {
-      background-color: var(--bg-pastel);
+    .page-contact .page-contact__backdrop {
+      display: block;
+      width: 58%;
+    }
+
+    .page-contact .page-contact__backdrop::after {
+      width: 0;
+      height: 0;
     }
 
     .page-contact__content {
       flex-direction: row;
-      background: var(--white);
-      padding: var(--spacing-larger) var(--spacing-large);
       justify-content: space-between;
+      padding: var(--spacing-larger) var(--spacing-large);
     }
 
     .page-contact .contact-sidebar {
@@ -82,7 +192,17 @@
     }
 
     .page-contact .contact-form {
-      width: 65%;
+      /* width: 65%; */
+    }
+
+    .page-contact__form-container {
+      display: flex;
+      align-items: center;
+    }
+
+    .page-contact__form-header {
+      height: 20rem;
+      width: 40%;
     }
   }
 
@@ -92,4 +212,68 @@
       padding: var(--spacing-larger) var(--spacing-larger);
     }
   }
+
+  .page-contact .page-contact__usps-title {
+    color: var(--black);
+  }
+
+  .page-contact .page-contact__usps-items {
+    position: relative;
+  }
+  
+  .page-contact .page-contact__usps-item-body {
+    display: block;
+  }
+
+  .page-contact .page-contact__usps-item-image {
+    display: block;
+    width: 60%;
+  }
+
+  .page-contact .page-contact__form-container {
+    margin-top: var(--spacing-small);
+    margin-bottom: var(--spacing-big);
+    padding-top: var(--spacing-medium);
+    background: linear-gradient(180deg, transparent 0%, var(--brand-yellow) 0%, var(--brand-yellow) 95%, transparent 0%);
+  }
+
+  .page-contact__form-header {
+    padding-top: var(--spacing-medium);
+    padding-bottom: var(--spacing-medium);
+  }
+
+  @media screen and (min-width: 720px) {
+    .page-contact .page-contact__form-container {
+      padding-top: var(--spacing-bigger);
+      margin-top: var(--spacing-big);
+    }
+
+    .page-contact .page-contact__form-container {
+      display: flex;
+      position: relative;
+      background: linear-gradient(180deg, transparent 30%, var(--brand-yellow) 0%, var(--brand-yellow) 80%, transparent 0%);
+    }
+
+    .page-contact .page-contact__form-container::after, 
+    .page-contact .page-contact__form-container::before {
+      content: "";
+      position: absolute;
+      width: 3rem;
+      height: 100%;
+      right: -2rem;
+      top: 0;
+      background: linear-gradient(180deg, transparent 30%, var(--brand-yellow) 0%, var(--brand-yellow) 80%, transparent 0%);
+      z-index: var(--z-index-low);
+    }
+
+    .page-contact .page-contact__form-container::before {
+      left: -3rem;
+    }
+  }
+
+  .page-contact__form-header {
+    margin-top: var(--spacing-medium);
+    margin-bottom: var(--spacing-medium);
+  }
+
 </style>
