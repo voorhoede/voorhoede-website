8fc891111415622adec37e0cce2eb78ccea107e5
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2022-03-08 15:26:51 +0100

    Embed Codepen Iframe (#489)

---
+++ src/client/components/code-pen-block/code-pen-block.vue
@@ -0,0 +1,55 @@
+<template>
+  <figure>
+    <iframe
+      scrolling="no"
+      :title="title"
+      :src="iframeSrc"
+      class="code-pen-block__i-frame"
+      frameborder="no"
+      loading="lazy"
+      allowtransparency="true"
+      allowfullscreen="true"
+    >
+    </iframe>
+    <figcaption class="code-pen-block__caption body-detail">
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    caption: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iframeSrc() {
+      const embedUrl = this.url.replace('/pen', '/embed/preview')
+      return `${embedUrl}?default-tab=css%2Cresult`
+    }
+  }
+}
+</script>
+
+<style>
+  .code-pen-block__i-frame {
+    width: 100%;
+    height: 25rem;
+  }
+
+  .code-pen-block__caption {
+    margin-top: var(--spacing-smaller);
+    text-align: center;
+  }
+</style>
--- src/client/pages/blog/_slug.vue
+++ src/client/pages/blog/_slug.vue
@@ -41,6 +41,16 @@
           />
         </div>
 
+        <code-pen-block
+          :id="item.id"
+          class="page-blog-post-list--full-width"
+          v-if="item.__typename === 'CodePenBlockRecord' && item.url"
+          :url="item.url"
+          :caption="item.caption"
+          :title="item.title"
+          :key="item.id"
+        />
+
         <code-block
           :id="item.id"
           class="page-blog-post-list--full-width"
