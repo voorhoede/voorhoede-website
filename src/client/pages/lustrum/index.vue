efef372db324b993d5135a2fc863c319c524d56c
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2022-05-20 11:46:04 +0200

    Set up the lustrum page

---
+++ src/client/pages/lustrum/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="grid">
+    <page-header
+      heading="byline"
+      :byline="page.title"
+      :headline="page.subtitle"
+      :image="page.headerIllustration"
+    />
+  </main>
+</template>
+
+<script>
+  import asyncData from '~/lib/async-page'
+  import head from '~/lib/seo-head'
+
+  export default {
+    asyncData,
+    mounted() {
+      this.$announcer.set(`${this.$t('page')}: ${this.page.social.title}`, 'polite')
+    },
+    head,
+  }
+</script>
+
+<style>
+
+</style>
