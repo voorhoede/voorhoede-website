ad64f1b85585be5c2aaa871ae913cc27a34f7a51
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2022-05-20 17:22:10 +0200

    Add list of facts to the lustrum page

---
+++ src/client/components/tag-item/tag-item.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tag-item body-big">
+    <strong>{{ amount }}</strong> {{ label }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      amount: {
+        type: Number,
+        required: true,
+      },
+      label: {
+        type: String,
+        required: true,
+      },
+    }
+  }
+</script>
+
+<style>
+  .tag-item {
+    padding: .2rem 1rem .1rem 1rem;
+    color: var(--html-blue);
+    background: var(--brand-yellow);
+    border-radius: 2rem;
+    white-space: nowrap;
+  }
+</style>
