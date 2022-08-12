1419bda5731ce53975b77ef4d343fd77e7288960
Vincent van Dijck <vincent@voorhoede.nl>
2022-08-12 16:00:22 +0200

    fix(seo-head): destructure data

--- src/client/lib/seo-head.js
+++ src/client/lib/seo-head.js
@@ -1,19 +1,21 @@
 export default function () {
-  const page = this.page
   if (!this.page) { return }
+
+  const { authors, customHead, social } = this.page
   const currentLocale = this.currentLocale
   const baseUrl = process.env.baseUrl
   const defaultShareTitle = 'De Voorhoede'
   const defaultShareImg = `${baseUrl}/images/social/logo-wide.jpg`
-  const pageTitle = page.social.title ? page.social.title : defaultShareTitle
+  const pageTitle = social.title ? social.title : defaultShareTitle
 
-  let socialDescription = page.social.description
-  if(page.authors) {
-    const authors = page.authors.map(author => {
+  let socialDescription = social.description
+
+  if (authors) {
+    const mappedAuthors = authors.map(author => {
       return author.twitterHandle ? author.twitterHandle : author.name
     }).join(' ')
 
-    socialDescription = `${socialDescription} by ${authors} @devoorhoede`
+    socialDescription = `${socialDescription} by ${mappedAuthors} @devoorhoede`
   }
 
   return {
@@ -22,12 +24,12 @@ export default function () {
     },
     title: pageTitle,
     meta: [
-      { name: 'description', content: page.social.description },
+      { name: 'description', content: social.description },
       { property: 'og:url', content: `${baseUrl}${this.$route.path}` },
       { property: 'og:type', content: 'website' },
       { property: 'og:title', content: pageTitle },
-      { property: 'og:description', content: page.social.description },
-      { property: 'og:image', content:  page.social.image ? page.social.image.url : defaultShareImg  },
+      { property: 'og:description', content: social.description },
+      { property: 'og:image', content:  social.image ? social.image.url : defaultShareImg  },
       { property: 'og:image:width', content: '1000' },
       { property: 'og:image:height', content: '500' },
       { property: 'og:site_name', content: 'www.voorhoede.nl' },
@@ -36,8 +38,8 @@ export default function () {
       { name: 'twitter:creator', content: '@devoorhoede' },
       { name: 'twitter:title', content: pageTitle },
       { name: 'twitter:description', content: socialDescription },
-      { name: 'twitter:image', content: page.social.image ? page.social.image.url : defaultShareImg },
+      { name: 'twitter:image', content: social.image ? social.image.url : defaultShareImg },
     ],
-    ...page.customHead,
+    ...customHead,
   }
 }
