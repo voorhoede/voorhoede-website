10a03b4a8da6345425ca0fe00b07b0de89adc23a
decrek <declanrek@gmail.com>
2022-12-20 17:18:13 +0100

    remove trailing slashes

--- config/nuxt/lib/routes.js
+++ config/nuxt/lib/routes.js
@@ -4,6 +4,6 @@ const pagesDir = './src/client/static/data/pages/'
 
 const routes = glob.sync(`${pagesDir}**/index.json`)
   .map(path => path.replace(pagesDir, '/'))
-  .map(path => path.replace('index.json', '/'))
+  .map(path => path.replace('index.json', ''))
 
 module.exports = routes
