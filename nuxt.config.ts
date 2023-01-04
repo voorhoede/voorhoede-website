c2c88b63358ce6001a981a38433709fd30e89d20
Selwyn <talk@selwyn.cc>
2023-01-04 12:31:05 +0100

    Move RSS feed generation to pre-build hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { writeFile, mkdir } from 'node:fs/promises';
 import { default as nuxtIcons } from 'nuxt-icons';
 import { default as plausible } from '@nuxtjs/plausible';
 import { fetchTranslations } from './src/scripts/fetch-translations.js';
+import { fetchBlogFeed } from './src/scripts/fetch-blog-feed';
 
 export default defineNuxtConfig({
   srcDir: 'src/client',
@@ -28,12 +29,19 @@ export default defineNuxtConfig({
     plausible,
   ],
   hooks: {
-    'build:before': () => (
+    'build:before': () => Promise.all([
       fetchTranslations({ datoApiToken: process.env.DATO_API_TOKEN })
         .then(async (translations) => {
           await mkdir('.cache', { recursive: true });
           await writeFile('.cache/ui-translations.json', JSON.stringify(translations));
-        })
-    ),
+        }),
+      fetchBlogFeed({ datoApiToken: process.env.DATO_API_TOKEN, baseUrl: process.env.BASE_URL })
+        .then(async (blogFeed) => {
+          await mkdir('./src/client/public/blog', { recursive: true });
+          await writeFile('./src/client/public/blog/feed.json', JSON.stringify(blogFeed));
+        }),
+    ])
+      // hook expects a promise with no return data
+      .then(() => {}),
   },
 });
