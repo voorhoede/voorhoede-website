03f8fd7b9e14b287f3e15f291c4236660a2cb19b
Selwyn <talk@selwyn.cc>
2023-02-15 11:51:10 +0100

    Add minimal ESLint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: [
+    'vue',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-undef': 'off',
+  },
+  overrides: [
+    {
+      files: [
+        '**/layouts/**/*.vue',
+        '**/pages/**/*.vue',
+        '**/error.vue',
+      ],
+      rules: {
+        'vue/multi-word-component-names': 'off',
+      },
+    },
+  ],
+};
