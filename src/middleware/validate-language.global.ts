4231baae6d2622a53f9c3234419e1f076bf7b13f
Selwyn <talk@selwyn.cc>
2023-03-28 14:48:40 +0200

    Validate locale by passing

--- src/middleware/validate-language.global.ts
+++ src/middleware/validate-language.global.ts
@@ -1,7 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { $i18n } = useNuxtApp();
+  console.info({to, from});
 
-  if (!$i18n.isValidLocale()) {
+  if (!$i18n.isValidLocale({ locale: to.params.language }) && from.path === to.path) {
+    console.warn('abortNavigation');
     return abortNavigation({ statusCode: 404 });
   }
 })
