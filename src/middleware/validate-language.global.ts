d6a3b563dd59815410c2936854786376aa9f41b4
Selwyn <talk@selwyn.cc>
2023-03-29 14:37:52 +0200

    Remove dev leftover

--- src/middleware/validate-language.global.ts
+++ src/middleware/validate-language.global.ts
@@ -2,7 +2,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const { $i18n } = useNuxtApp();
 
   if (!$i18n.isValidLocale({ locale: to.params.language })) {
-    console.warn('abortNavigation');
     return abortNavigation({ statusCode: 404, fatal: true });
   }
 })
