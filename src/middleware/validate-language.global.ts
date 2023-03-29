a6eccd10f0bf50bf8ce9c249df5e403bc1ee5175
Selwyn <talk@selwyn.cc>
2023-03-29 13:06:49 +0200

    Handle navigating back

--- src/middleware/validate-language.global.ts
+++ src/middleware/validate-language.global.ts
@@ -1,8 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { $i18n } = useNuxtApp();
-  console.info({to, from});
 
-  if (!$i18n.isValidLocale({ locale: to.params.language }) && from.path === to.path) {
+  if (!$i18n.isValidLocale({ locale: to.params.language })) {
     console.warn('abortNavigation');
     return abortNavigation({ statusCode: 404, fatal: true });
   }
