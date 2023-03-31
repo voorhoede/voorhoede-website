0845dca1acf90a9aa9d2b12807a2331755249f94
Sjoerd Beentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2023-03-31 13:36:29 +0200

    fetch routes to prerender instead of crawling (#587)
    * Add fetchRoutes function and invocation to prerender hook for dynamic routes generation
    
    * move /energy-first/ to static routes, since it's not dynamically generated
    
    * rename people -> team
    
    * add contact confirmation & jobs pages to dynamic routes
    
    * rename model -> operation, since 'operation' is more descriptive
    
    * get locale codes from i18n.js, since the locales are already defined there
    
    * convert fetch-routes script to typescript
    
    * fix formatting of fetch-routes script

---
+++ src/scripts/fetch-routes.ts
@@ -0,0 +1,192 @@
+import { locales } from "../lib/i18n";
+
+type RouteConfig = {
+  queryOperation: string;
+  path: string;
+};
+
+type Locale = {
+  code: string;
+  name: string;
+}
+
+// these route are static and will not need any further processing
+const staticRoutesConfig = [
+  "/",
+  "/blog/",
+  "/services/",
+  "/contact/",
+  "/cases/",
+  "/jobs/",
+  "/work-at/",
+  "/lustrum/",
+  "/events/",
+  "/faq/",
+  "/energy-first/",
+];
+
+// these routes are generated dynamically by fetching all slugs for each page type
+// the slugs are appended to the value of the 'baseRoute' property
+const dynamicRoutesConfig: RouteConfig[] = [
+  {
+    queryOperation: "allServices",
+    path: "/services/",
+  },
+  {
+    queryOperation: "allBlogPosts",
+    path: "/blog/",
+  },
+  {
+    queryOperation: "allCaseItems",
+    path: "/cases/",
+  },
+  {
+    queryOperation: "allPeople",
+    path: "/team/",
+  },
+  {
+    queryOperation: "allEventItems",
+    path: "/events/",
+  },
+  {
+    queryOperation: "allContactConfirmations",
+    path: "/contact/",
+  },
+  {
+    queryOperation: "allJobs",
+    path: "/jobs/",
+  },
+];
+
+// fetches data from DatoCMS using the GraphQL API
+const fetchDatoQuery = ({
+  query,
+  variables = {},
+}: {
+  query: string;
+  variables?: Record<string, unknown>;
+}) => {
+  return fetch("https://graphql.datocms.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: process.env.DATO_API_TOKEN,
+    } as HeadersInit,
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+    .then((res) => res.json())
+    .then(({ data, errors }) => {
+      if (errors) {
+        console.log(errors);
+      }
+
+      return data;
+    });
+};
+
+// fetches a paginated list of slugs for a given operation
+const fetchPaginatedSlugsForOperation = ({
+  operation,
+  locale,
+  skip,
+}: {
+  operation: string;
+  locale: string;
+  skip: number;
+}) => {
+  return fetchDatoQuery({
+    query: `
+        query ${operation}($skip: IntType, $locale: SiteLocale) {
+            ${operation}(first: 100, skip: $skip, locale: $locale) {
+                slug
+            }
+        }
+    `,
+    variables: {
+      locale,
+      skip,
+    },
+  }).then((data) => data[operation]);
+};
+
+// fetches the total number of items for a given operation
+const fetchMetaForOperation = ({
+  operation,
+  locale,
+}: {
+  operation: string;
+  locale: string;
+}) => {
+  return fetchDatoQuery({
+    query: `
+        query Meta ($locale: SiteLocale) {
+            _${operation}Meta(locale: $locale) {
+                count
+            }
+        }
+    `,
+    variables: {
+      locale,
+    },
+  });
+};
+
+// fetches all slugs for operation
+const fetchSlugsForOperation = async ({
+  operation,
+  locale,
+}: {
+  operation: string;
+  locale: string;
+}) => {
+  const meta = await fetchMetaForOperation({ operation, locale });
+  const { count } = meta[`_${operation}Meta`];
+  const pages = Math.ceil(count / 100);
+
+  return Promise.all(
+    [...Array(pages)].map((_, index) =>
+      fetchPaginatedSlugsForOperation({ operation, locale, skip: index * 100 })
+    )
+  ).then((data) => data.flat().map((data) => data.slug));
+};
+
+// fetches all routes for a given locale
+const fetchDynamicRoutes = ({
+  locale,
+  dynamicRoutesConfig,
+}: {
+  locale: string;
+  dynamicRoutesConfig: RouteConfig[];
+}) => {
+  return Promise.all(
+    dynamicRoutesConfig.map(async ({ queryOperation, path }) => {
+      const slugs = await fetchSlugsForOperation({
+        operation: queryOperation,
+        locale,
+      });
+
+      return slugs.map((slug) => `/${locale}${path}${slug}/`);
+    })
+  ).then((data) => data.flat());
+};
+
+export const fetchRoutes = () =>
+  Promise.all(
+    locales
+      .map((locale: Locale) => locale.code)
+      .map(async (locale: string) => {
+        const dynamicRoutes = await fetchDynamicRoutes({
+          locale,
+          dynamicRoutesConfig,
+        });
+
+        return [
+          ...staticRoutesConfig.map((route) => `/${locale}${route}`),
+          ...dynamicRoutes,
+        ];
+      })
+  ).then((data) => data.flat());
