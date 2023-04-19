38fb49856827fd26670ebbb6fc5c6ed8001561c8
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2023-04-19 15:17:06 +0200

    fix reactivity issues

--- src/composables/usei18nSlugs.js
+++ src/composables/usei18nSlugs.js
@@ -1,7 +1,10 @@
 export async function useI18nSlugs() {
     const route = useRoute()
+    const key = [route.name, route.params.slug, 'i18n-slugs']
+        .filter(Boolean)
+        .join('-')
 
-    const { data } = await useAsyncData(`${route.name}-${route.params.slug}-i18n-slugs`, async () => {
+    const { data } = await useAsyncData(key, async () => {
         const i18nRouteConfig = (await import('../../.cache/i18n-slugs.json')).default
         const i18nSlugs = i18nRouteConfig.find((i18nRoute) => i18nRoute.route === route.name)
 
@@ -10,6 +13,8 @@ export async function useI18nSlugs() {
         }
 
         return i18nSlugs.slugs.find(localeConfig => localeConfig.find(({ locale, value }) => value === route.params.slug && locale === route.params.language))
+    }, {
+        watch: [route]
     })
 
     return data
