77a62f394dd624e88ec4770f0c152744652666e1
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2023-04-21 15:02:33 +0200

    use defineNuxtLink for default trailing slash in app-link

--- src/components/app-link/app-link.vue
+++ src/components/app-link/app-link.vue
@@ -1,30 +1,11 @@
 <script setup>
-  import { withTrailingSlash } from 'ufo';
-
-  const props = defineProps({
-    to: {
-      type: [String, Object],
-      required: true,
-    },
-  });
-  const router = useRouter();
-
-  const resolvedRoute = computed(() => router.resolve(props.to));
-  const normalizedPath = computed(() =>  withTrailingSlash(
-    typeof props.to === 'object' ? resolvedRoute.value.path : props.to
-  ));
-  const normalizedTo = computed(() =>
-    typeof props.to === "object"
-      ? {
-          ...resolvedRoute.value,
-          path: normalizedPath.value,
-        }
-      : normalizedPath.value
-  );
+  const LinkWithTrailingSlash = defineNuxtLink({
+    trailingSlash: 'append'
+  })
 </script>
 
 <template>
-  <nuxt-link :to="normalizedTo">
+  <LinkWithTrailingSlash>
     <slot />
-  </nuxt-link>
+  </LinkWithTrailingSlash>
 </template>
