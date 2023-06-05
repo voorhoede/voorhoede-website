66a45c630aadbe4912f15c65c2a07dd1d4672229
joanpadolina <joanpadolina@gmail.com>
2023-06-05 14:41:37 +0200

    refactor(team-gallery): with different position gallery item

--- src/components/team-gallery/team-gallery.vue
+++ src/components/team-gallery/team-gallery.vue
@@ -8,7 +8,7 @@
       >
         <app-link
           :to="$localeUrl({ name: 'team-slug', params: { slug: member.slug } })"
-          class=""
+          class="team-gallery-member__link"
         >
           <dato-image
             class="team-gallery-member__image"
@@ -19,13 +19,16 @@
             loading="lazy"
             sizes="(min-width: 800px) 20vw, (min-width: 500px) 33vw, 50vw"
           />
-          <div class="gallery-item__details">
-            <p class="h4">
-              {{ member.name }}
-            </p>
-            <span class="body-detail">{{ member.jobTitle }}</span>
-          </div>
         </app-link>
+        <div class="team-gallery-member__details">
+          <p class="h4 team-gallery-member__name">
+            {{ member.name }}
+          </p>
+          <span
+            v-if="member.jobTitle"
+            class="body-detail team-gallery-member__job"
+          >{{ member.jobTitle }}</span>
+        </div>
       </li>
     </ul>
   </section>
@@ -51,31 +54,75 @@ defineProps<{
 <style>
 :root {
   --gallery-parallax-gap: 10px;
+  --team-gallery-grid-columns: 1;
 }
 
 .team-gallery__list {
-  columns: 3 200px;
+  display: grid;
+  grid-template-columns: repeat(var(--team-gallery-grid-columns), 1fr);
+  /* grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); */
+  gap: 10px;
+}
+
+.team-gallery-member__link {
+  display: block;
+  height: auto;
 }
 
 .team-gallery__list-item {
-  /*display: inline-flex;
-		height: auto;
-		flex-grow: 0;
-		flex-shrink: 1;*/
-  /* width: calc(50% - var(--gallery-parallax-gap)); */
-  height: 26rem;
-  margin-bottom: 1rem;
+  /* text-align: center; */
+  position: relative;
 }
 
-.team-gallery__list-item:nth-child(n+4):nth-child(-n+8),
-.team-gallery__list-item:nth-child(n+9):nth-child(-n+16) {
-  border: 2px solid red;
-  height: 20rem;
+@media (min-width: 486px) {
+  :root {
+    --team-gallery-grid-columns: 2;
+  }
+
+  .team-gallery__list {
+    grid-template-columns: repeat(var(--team-gallery-grid-columns), 1fr);
+  }
+  .team-gallery__list-item:nth-child(2n+2){
+    transform: translateY(20px);
+  }
 }
 
-@media (min-width: 500px) {
-  .team-gallery__list-item {
-    /* width: calc(33% - var(--gallery-parallax-gap)); */
+@media (min-width: 768px) {
+  :root {
+    --team-gallery-grid-columns: 3;
+  }
+
+  .team-gallery__list .team-gallery__list-item:nth-child(2n+2){
+    transform: translateY(0px);
+  }
+
+  .team-gallery__list .team-gallery__list-item:nth-child(3n+2) {
+    transform: translateY(50px);
+  }
+}
+
+@media (min-width: 1020px) {
+  :root {
+    --team-gallery-grid-columns: 4;
+  }
+
+  .team-gallery__list .team-gallery__list-item:nth-child(2n+2){
+    transform: translateY(0px);
+  }
+
+  .team-gallery__list .team-gallery__list-item:nth-child(3n+2) {
+    transform: translateY(0px);
+  }
+
+  .team-gallery__list .team-gallery__list-item:nth-child(4n+2),
+  .team-gallery__list .team-gallery__list-item:nth-child(4n+4) {
+    transform: translateY(50px);
+  }
+}
+
+@media (min-width: 1200px) {
+  :root {
+    --team-gallery-grid-columns: 6;
   }
 }
 
@@ -83,5 +130,24 @@ defineProps<{
   display: block;
   height: 100%;
   object-fit: cover;
+}
+
+.team-gallery-member__details {
+  position: absolute;
+  background-color: var(--brand-yellow);
+  z-index: 1;
+  bottom: 0;
+  right: 0;
+  padding: 10px;
+}
+
+.team-gallery-member__name {
+  /* background-color: var(--brand-yellow); */
+  padding-top: 10px;
+}
+
+.team-gallery-member__job {
+  display: block;
+  /* color: var(--white); */
 }
 </style>
