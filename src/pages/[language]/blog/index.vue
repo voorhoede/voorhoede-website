290d81ff789f8fe1631d33be4b36dee736861cc8
Frank Warnaar <frank@voorhoede.nl>
2023-07-05 16:23:48 +0200

    feat(blog/index page): re-export paginated page

--- src/pages/[language]/blog/index.vue
+++ src/pages/[language]/blog/index.vue
@@ -1,10 +1,3 @@
-<script setup>
-definePageMeta({
-  middleware: [
-    // eslint-disable-next-line no-unused-vars
-    function (to, from) {
-      return navigateTo({ name: 'language-blog-page-page', params: { page: 1 }})
-    },
-  ],
-});
+<script>
+export { default } from './page/[page].vue'
 </script>
