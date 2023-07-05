72763ff6650a9590ac4d548b7b9833d358761c6d
Frank Warnaar <frank@voorhoede.nl>
2023-07-05 10:37:20 +0200

    chore: use migrated deploy env

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,2 +1,2 @@
-export const datocmsEnvironment = 'image-cards-refactor';
+export const datocmsEnvironment = 'image-cards-refactor-deploy';
 export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
