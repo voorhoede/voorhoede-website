a4e7eccac6f851c8b35a3d373bc5a69ebd9ff62a
Lucia Sarni <lucia@voorhoede.nl>
2023-07-11 14:42:42 +0200

    Deprecate image-with-description

--- migrations/1689065480_updateImageBlock.ts
+++ migrations/1689065480_updateImageBlock.ts
@@ -26,4 +26,21 @@ export default async function (client: Client) {
     },
     default_value: "top",
   });
+
+  console.log("Finalize models/block models");
+
+  console.log(
+    'Update model "[Deprecated] Image with description" (`image_with_description`)'
+  );
+  await client.itemTypes.update("41923", {
+    name: "[Deprecated] Image with description",
+  });
+
+  console.log(
+    'Update block model "[Deprecated] Image with text" (`image_with_text`)'
+  );
+  await client.itemTypes.update("41924", {
+    name: "[Deprecated] Image with text",
+    hint: "This block type is deprecated, please use Image instead.",
+  });
 }
