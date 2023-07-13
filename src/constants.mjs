3e95d947dc7ef6dbbfa927d88e594cb5e06d5078
Lucia Sarni <lucia@voorhoede.nl>
2023-07-13 14:03:12 +0200

    Update constants

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,2 +1,2 @@
-export const datocmsEnvironment = 'image-with-caption';
+export const datocmsEnvironment = 'image-with-caption-deploy';
 export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
