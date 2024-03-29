2113167509c39b830edea938ce65493484db5435
Jamerrone <james.p.perrone@protonmail.com>
2024-03-29 11:39:50 +0100

    feat: update datocmsEnvironment

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,2 +1,2 @@
-export const datocmsEnvironment = 'implement-google-calendar-schedular';
+export const datocmsEnvironment = 'implement-google-calendar-schedular-deploy';
 export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
