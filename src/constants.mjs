75eae903b75447c13e2976cae799e4f59c334b2c
Wessel Smit <45405413+WesselSmit@users.noreply.github.com>
2024-06-14 14:29:47 +0200

    set correct environment (#805)

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,2 +1,2 @@
-export const datocmsEnvironment = 'cookie-notice';
+export const datocmsEnvironment = 'youtube-cookie-notice-deploy';
 export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
