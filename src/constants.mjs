2adbb51ecf57b3d7dc1d752e0750c922c0af68d0
wesselsmit <wrsmit00@gmail.com>
2024-06-14 15:20:30 +0200

    feat: remove heading mapping in blog posts

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,2 +1,2 @@
-export const datocmsEnvironment = 'youtube-cookie-notice-deploy';
+export const datocmsEnvironment = 'blog-headings';
 export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
