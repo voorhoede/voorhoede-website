74973eb2a0ae592e94676565530972440899f798
Selwyn <talk@selwyn.cc>
2024-08-22 14:57:37 +0200

    Update all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 import { defineNuxtConfig } from 'nuxt/config';
-import { writeFile, mkdir } from 'node:fs/promises';
+import { mkdir, writeFile } from 'node:fs/promises';
 import { default as nuxtIcons } from 'nuxt-icons';
 import { default as plausible } from '@nuxtjs/plausible';
 import { fetchTranslations } from './src/scripts/fetch-translations';
@@ -9,6 +9,7 @@ import { fetchRoutes } from './src/scripts/fetch-routes';
 import { fetchI18nSlugs } from './src/scripts/fetch-i18n-slugs';
 
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-22',
   srcDir: 'src',
   typescript: {
     shim: false,
@@ -18,14 +19,14 @@ export default defineNuxtConfig({
   ],
   nitro: {
     prerender: {
-      crawlLinks: false
-    }
+      crawlLinks: false,
+    },
   },
   runtimeConfig: {
     public: {
       datoApiToken: process.env.DATOCMS_API_READ_TOKEN,
       baseUrl: process.env.BASE_URL,
-      previewSecret: process.env.PREVIEW_SECRET
+      previewSecret: process.env.PREVIEW_SECRET,
     },
   },
   modules: [
@@ -42,29 +43,36 @@ export default defineNuxtConfig({
           generatedRoutes.forEach((route) => {
             // routes is of type Set, so we need to add each route individually
             routes.add(route);
-          })
-        })
+          });
+        });
     },
-    'build:before': () => Promise.all([
-      fetchTranslations()
-        .then(async (translations) => {
-          await mkdir('.cache', { recursive: true });
-          await writeFile('.cache/ui-translations.json', JSON.stringify(translations));
-        }),
-      fetchBlogFeed()
-        .then(async (blogFeed) => {
-          await mkdir('./src/public/blog', { recursive: true });
-          await writeFile('./src/public/blog/feed.json', JSON.stringify(blogFeed));
-        }),
-      fetchRedirects()
-        .then((redirects) => writeFile('./src/public/_redirects', redirects)),
-      fetchI18nSlugs()
-        .then(async (data) => {
-          await mkdir('.cache', { recursive: true });
-          await writeFile('.cache/i18n-slugs.json', JSON.stringify(data));
-        }),
-    ])
-      // hook expects a promise with no return data
-      .then(() => {}),
+    'build:before': () =>
+      Promise.all([
+        fetchTranslations()
+          .then(async (translations) => {
+            await mkdir('.cache', { recursive: true });
+            await writeFile(
+              '.cache/ui-translations.json',
+              JSON.stringify(translations),
+            );
+          }),
+        fetchBlogFeed()
+          .then(async (blogFeed) => {
+            await mkdir('./src/public/blog', { recursive: true });
+            await writeFile(
+              './src/public/blog/feed.json',
+              JSON.stringify(blogFeed),
+            );
+          }),
+        fetchRedirects()
+          .then((redirects) => writeFile('./src/public/_redirects', redirects)),
+        fetchI18nSlugs()
+          .then(async (data) => {
+            await mkdir('.cache', { recursive: true });
+            await writeFile('.cache/i18n-slugs.json', JSON.stringify(data));
+          }),
+      ])
+        // hook expects a promise with no return data
+        .then(() => {}),
   },
 });
