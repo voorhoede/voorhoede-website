0d9c0a72e77714e02cd6ed1fe5b901858365ee7f
Selwyn <talk@selwyn.cc>
2025-01-03 13:54:29 +0100

    Move Netlify pretty URLs to Nuxt middleware

---
+++ src/middleware/trailing-slash.global.ts
@@ -0,0 +1,7 @@
+import { withTrailingSlash } from "ufo";
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (!to.path.endsWith("/")) {
+    return navigateTo(withTrailingSlash(to.fullPath, true));
+  }
+});
