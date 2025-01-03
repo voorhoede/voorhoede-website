1fde16d3d356dd081522dfef192be55b4b00cf6d
Selwyn <talk@selwyn.cc>
2025-01-03 17:58:56 +0100

    Use our own image component

--- src/components/app-image/types.ts
+++
@@ -1,5 +0,0 @@
-export type ImageLoader = (resolverProps: {
-  src: string;
-  width: number;
-  quality?: number;
-}) => string;
--- src/components/dato-image/dato-image.vue
+++ src/components/dato-image/dato-image.vue
@@ -1,5 +1,5 @@
 <template>
-  <app-image
+  <Image
     class="dato-image"
     :src="props.src"
     :loader="loader"
@@ -8,36 +8,37 @@
 </template>
 
 <script setup lang="ts">
-  import { withQuery } from 'ufo';
-  import { type ImageLoader } from '../app-image/types';
-  import type { ImgixUrl } from 'typescript-imgix-url-params';
+import { withQuery, type QueryObject } from "ufo";
+import type { ImgixUrl } from "typescript-imgix-url-params";
+import { Image, type ImageLoader } from "@voorhoede/image-vue";
 
-  const props = defineProps<{
-    src: string,
-    srcset?: string,
-    modifiers?: ImgixUrl.Params,
-  }>();
+const props = defineProps<{
+  src: string;
+  srcset?: string;
+  modifiers?: ImgixUrl.Params;
+}>();
 
-  const imgixLoader: ImageLoader = ({ src, width, quality }) => (
-    withQuery(src, { w: width, q: quality, auto: 'format,compress', ...props.modifiers })
-  );
+const imgixLoader: ImageLoader = ({ src, width, quality }) =>
+  withQuery(src, {
+    w: width,
+    q: quality,
+    auto: "format,compress",
+    cs: "origin",
+    ...(props.modifiers as QueryObject),
+  });
 
-  const loader = computed(() => (
-    props.src.endsWith('.svg')
-      ? undefined
-      : imgixLoader
-  ));
+const loader = computed(() =>
+  props.src.endsWith(".svg") ? ({ src }: { src: string }) => src : imgixLoader
+);
 
-  const checkedSrcset = computed(() => (
-    props.src.endsWith('.svg')
-      ? ''
-      : props.srcset
-  ));
+const checkedSrcset = computed(() =>
+  props.src.endsWith(".svg") ? "" : props.srcset
+);
 </script>
 
 <style>
-  .dato-image {
-    display: block;
-    max-width: 100%;
-  }
+.dato-image {
+  display: block;
+  max-width: 100%;
+}
 </style>
