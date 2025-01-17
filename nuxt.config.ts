4ece73efdb4d6af3482bb9aaeb7760097345176d
Selwyn <talk@selwyn.cc>
2025-01-17 14:18:49 +0100

    Use crawler instead of custom route fetching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ import { default as plausible } from '@nuxtjs/plausible';
 import { fetchTranslations } from './src/scripts/fetch-translations';
 import { fetchBlogFeed } from './src/scripts/fetch-blog-feed';
 import { fetchRedirects } from './src/scripts/fetch-redirects';
-import { fetchRoutes } from './src/scripts/fetch-routes';
 import { fetchI18nSlugs } from './src/scripts/fetch-i18n-slugs';
 import { defaultLanguage } from './src/lib/i18n';
 
@@ -21,8 +20,7 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'netlify',
     prerender: {
-      crawlLinks: false,
-      // need at least one route to trigger the 'prerender:routes' hook
+      crawlLinks: true,
       routes: [`/${defaultLanguage}/`],
       concurrency: 35, // stay below 40 to avoid rate limiting
       interval: 1000, // use 1 second interval to avoid rate limiting
@@ -32,7 +30,9 @@ export default defineNuxtConfig({
       '/blog-feed.xml': {
         redirect: { to: '/blog/feed.json', statusCode: 301 },
       },
-      '/mogelijk/api/event': { proxy: { to:  'https://plausible.io/api/event' } },
+      '/mogelijk/api/event': {
+        proxy: { to: 'https://plausible.io/api/event' },
+      },
     },
   },
   runtimeConfig: {
@@ -50,15 +50,6 @@ export default defineNuxtConfig({
     apiHost: '/mogelijk',
   },
   hooks: {
-    'prerender:routes': ({ routes }) => {
-      fetchRoutes()
-        .then((generatedRoutes) => {
-          generatedRoutes.forEach((route) => {
-            // routes is of type Set, so we need to add each route individually
-            routes.add(route);
-          });
-        });
-    },
     'build:before': () =>
       Promise.all([
         fetchTranslations()
