179a08a9d41f9b0c17024b30798606353d0e53c1
Selwyn <talk@selwyn.cc>
2025-01-17 14:01:45 +0100

    Correctly pass datocms environment name
    Downgrade datocms-listen to 1.0.0 to fix environment name header.

--- src/composables/useFetchContent.js
+++ src/composables/useFetchContent.js
@@ -25,7 +25,6 @@ export async function useFetchContent({ key = null, query, variables }) {
         onUpdate: ({ response }) => { data.value = response.data; },
         onError: console.error,
         onChannelError: console.error,
-        preview: true,
       });
     });
 
