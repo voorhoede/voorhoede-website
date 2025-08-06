67932d952fc05022e423aca28cd57a2c48e788f8
MarleenEliza <marleen@voorhoede.nl>
2025-08-06 09:33:47 +0200

    use pipedrive keys

--- server/api/newsletter.post.ts
+++ server/api/newsletter.post.ts
@@ -31,64 +31,18 @@ export default defineEventHandler(async (event) => {
     });
 });
 
-type PersonFieldData = {
-  id: number;
-  key: string;
-  name: string;
-  options: {
-    label: string;
-    id: number;
-  }[];
-};
+/**
+ * The API key for the 'Contact Source' field.
+ * This API key is can be checked in the Pipedrive UI.
+ */
+const CONTACT_SOURCE_FIELD_APIKEY = "777843b709aa29ae549063f689f7a6bd4c06b481";
 
 /**
- * Below naming conventions should NOT be changed in Pipedrive.
- * It is a bit of a hassle, but PipeDrive ids and keys cannot be adjusted.
+ * The ID of the 'Newsletter (Website)' option in the 'Contact Source' field.
+ * This ID is cannot be checked in the Pipedrive UI.
+ * Based on light testing with removing and adding option fields, I assume this ID will remain the same but there is no guarantee.
  */
-const CONTACT_SOURCE_IDENTIFIER = "contact source";
-const WEBSITE_OPTION_IDENTIFIER = "newsletter";
-
-/**
- * Fetches all person fields from Pipedrive and extracts the key for the 'Contact Source' field
- * and the option id for 'Newsletter (Website)'.
- * PipeDrive ids and keys cannot be adjusted hence the need to first GET all the fields.
- *
- * @param params - Object containing the Pipedrive API token and base URL
- * @returns An object with the Contact Source field key and the Newsletter (Website) option id, or empty object if info cannot be found
- */
-async function getContactSourceFieldInfo({
-  api_token,
-  url,
-}: {
-  api_token: string;
-  url: string;
-}): Promise<{ [key: string] : number }> {
-  const params = new URLSearchParams({ api_token });
-  const result: { success: boolean; data: PersonFieldData[] } = await fetch(
-    new URL(`${url}/v1/personFields?${params}`),
-  ).then((response) => response.json());
-  const contactSourceField = result.data.find((field) =>
-    field.name.toLowerCase().includes(CONTACT_SOURCE_IDENTIFIER),
-  );
-  if (!contactSourceField) return {};
-  const newsLetterOptionId = getNewsLetterOptionId(contactSourceField.options);
-  if (!newsLetterOptionId) return {};
-  return { [contactSourceField.key]: newsLetterOptionId };
-}
-
-/**
- * Finds the id of the 'Newsletter (Website)' option in a list of field options.
- *
- * @param options - Array of field options from a Pipedrive options field
- * @returns The id of the Newsletter (Website) option, or undefined if not found
- */
-function getNewsLetterOptionId(
-  options: PersonFieldData["options"],
-): number | undefined {
-  return options.find((option) =>
-    option.label.toLowerCase().includes(WEBSITE_OPTION_IDENTIFIER),
-  )?.id;
-}
+const NEWSLETTER_OPTION_ID = 309;
 
 async function existingUserIdByEmail(
   { email }: { email: string },
@@ -112,10 +66,6 @@ async function subscribe(
 ) {
   const id = await existingUserIdByEmail({ email }, { api_token, url });
   const params = new URLSearchParams({ api_token });
-  const contactSourceFieldInfo = await getContactSourceFieldInfo({
-    api_token,
-    url,
-  });
 
   if (id) {
     // Do not update the user if they do not give consent
@@ -126,7 +76,7 @@ async function subscribe(
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         marketing_status: 'subscribed',
-        ...contactSourceFieldInfo,
+        [CONTACT_SOURCE_FIELD_APIKEY]: NEWSLETTER_OPTION_ID,
       }),
     });
   }
@@ -139,7 +89,7 @@ async function subscribe(
       name,
       email: [{ value: email, primary: true, label: 'work' }],
       marketing_status: consent ? 'subscribed' : 'no_consent',
-      ...contactSourceFieldInfo,
+      [CONTACT_SOURCE_FIELD_APIKEY]: NEWSLETTER_OPTION_ID,
     }),
   });
 }
