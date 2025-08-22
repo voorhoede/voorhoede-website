d750751a6e0bda932ec8e9f72c30f540f893bf04
Selwyn <talk@selwyn.cc>
2025-08-22 10:59:56 +0200

    Check for contact post spam with Akismet

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -4,38 +4,71 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   if (
-    !body?.name || !body?.email || !body.explanation || body[honeypotFieldName]
+    !body?.name ||
+    !body?.email ||
+    !body.explanation ||
+    body[honeypotFieldName]
   ) {
     return sendRedirect(event, '/en/contact/failed/');
   }
 
-  return fetch(
-    'https://api.postmarkapp.com/email',
-    {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-Postmark-Server-Token': config.postmarkApiToken,
-      },
-      body: JSON.stringify({
-        'MessageStream': 'contact-form',
-        'From': `"${body.name}" <forms@voorhoede.nl>`,
-        'To': 'post@voorhoede.nl',
-        'Subject': 'Voorhoede website contact form',
-        'TextBody': [
-          ['From:', body.email],
-          body.business && ['Company:', body.business],
-          body.phone && ['Phone:', body.phone],
-          ['Body:', body.explanation],
-        ]
-          .filter(Boolean)
-          .map((pair) => [...pair, '\n'])
-          .flat()
-          .join('\n'),
-      }),
+  const akismetData = new URLSearchParams({
+    api_key: config.akismetApiToken,
+    blog: config.public.baseUrl,
+    user_ip: getRequestIP(event) || '',
+    user_agent: getHeader(event, 'user-agent') || '',
+    referrer: getHeader(event, 'referer') || '',
+    comment_type: 'contact-form',
+    comment_author: body.name,
+    comment_author_email: body.email,
+    comment_content: body.explanation,
+  });
+
+  const isSpam = await fetch('https://rest.akismet.com/1.1/comment-check', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  )
+    body: akismetData.toString(),
+  }).then((response) => response.text());
+
+  if (isSpam === 'true') {
+    return sendRedirect(event, '/en/contact/failed/');
+  }
+
+  console.info('Contact post data', '\n', {
+    userIp: akismetData.get('user_ip'),
+    userAgent: akismetData.get('user_agent'),
+    referrer: akismetData.get('referrer'),
+    commentAuthor: akismetData.get('comment_author'),
+    isSpam,
+  });
+
+  return fetch('https://api.postmarkapp.com/email', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-Postmark-Server-Token': config.postmarkApiToken,
+    },
+    body: JSON.stringify({
+      MessageStream: 'contact-form',
+      From: `"${body.name}" <forms@voorhoede.nl>`,
+      To: 'post@voorhoede.nl',
+      Subject: 'Voorhoede website contact form',
+      TextBody: [
+        ['From:', body.email],
+        ['Name:', body.name],
+        body.business && ['Company:', body.business],
+        body.phone && ['Phone:', body.phone],
+        ['Body:', body.explanation],
+      ]
+        .filter(Boolean)
+        .map((pair) => [...pair, '\n'])
+        .flat()
+        .join('\n'),
+    }),
+  })
     .then(() => {
       return sendRedirect(event, '/en/contact/confirmation/', 303);
     })
