27aa13016c5c02ba448e5f3c8bd332538a5954a2
Selwyn <talk@selwyn.cc>
2026-05-13 11:14:56 +0200

    Cleanup models and new blocks, fix footer

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,2 +1,2 @@
-export const datocmsEnvironment = "home-page-deploy";
-export const mastodonUrl = "https://fosstodon.org/@devoorhoede";
+export const datocmsEnvironment = 'models-housekeeping';
+export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
