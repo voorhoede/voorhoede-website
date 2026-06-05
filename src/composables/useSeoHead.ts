a02ef300e9898f0f065cb780ed6d172e72d1ee21
Selwyn <talk@selwyn.cc>
2026-06-05 15:10:45 +0200

    Setup new home page

--- src/composables/useSeoHead.ts
+++ src/composables/useSeoHead.ts
@@ -1,7 +1,17 @@
 import { withTrailingSlash } from 'ufo';
 import { mastodonUrl } from '../constants.mjs';
 
-export function useSeoHead({ title, i18nSlugs, social }) {
+export function useSeoHead(
+  {
+    title,
+    i18nSlugs,
+    social,
+  }: {
+    title: string;
+    i18nSlugs?: unknown;
+    social: Record<string, string | unknown>;
+  },
+) {
   if (!social) {
     throw new Error('Missing required SEO data');
   }
@@ -20,11 +30,12 @@ export function useSeoHead({ title, i18nSlugs, social }) {
   useHead({
     title: social.title || title,
     meta: [
-      { name: 'description', content: social.description },
+      { name: 'description', content: String(social.description) },
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: pageUrl },
-      { property: 'og:title', content: social.title || title || 'De Voorhoede' },
-      { property: 'og:description', content: social.description },
+      { property: 'og:title', content: String(social.title || title || 'De Voorhoede') },
+      { property: 'og:description', content: String(social.description) },
+      // @ts-expect-error social is not typed
       { property: 'og:image', content: social.image?.url || defaultShareImg },
       { property: 'og:image:width', content: '1000' },
       { property: 'og:image:height', content: '500' },
@@ -40,7 +51,8 @@ export function useSeoHead({ title, i18nSlugs, social }) {
         }
         : null,
       ...$i18n.locales
-        .map(({ code }) => {
+        .map(({ code }: { code: string }) => {
+          // @ts-expect-error i18nSlugs is not typed
           const alternateSlug = i18nSlugs?.find((i18nSlug) => i18nSlug.locale === code)?.value;
           if (i18nSlugs && !alternateSlug) {
             return null;
