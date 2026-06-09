47d76527160376b146758bb7d18af399a954e9b3
Selwyn <talk@selwyn.cc>
2026-06-09 16:51:12 +0200

    Add back legacy components temporarily

---
+++ src/components/dialogue-cta/dialogue-cta.vue
@@ -0,0 +1,150 @@
+<template>
+  <section class="grid dialogue-cta" v-once>
+      <div class="dialogue-cta__image">
+        <dato-image
+          :src="person.image.url"
+          alt=""
+          :width="200"
+          :height="200"
+          loading="eager"
+          :quality="75"
+        />
+      </div>
+      <div>
+        <h2 class="dialogue-cta__title h4">
+          {{ title }}
+        </h2>
+        <div
+          v-if="variant === 'information'"
+          class="dialogue-cta__body body-big"
+          v-html="body"
+        />
+        <blockquote
+          v-if="variant === 'quote'"
+          class="dialogue-cta__body dialogue-cta__body--quote body-big"
+          v-html="body"
+        />
+        <div class="dialogue-cta__ctas">
+          <app-button
+            v-for="(cta, index) in ctas"
+            :key="index"
+            :label="cta.label"
+            :to="cta.to"
+            :external="cta.external"
+            :primary="cta.style === 'primary'"
+            :secondary="cta.style === 'secondary'"
+          />
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    person: {
+      type: Object,
+      required: true,
+    },
+    ctas: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+.dialogue-cta {
+  display: flex;
+  flex-direction: column;
+  gap: clamp(var(--spacing-medium), 8%, var(--spacing-larger));
+}
+
+.dialogue-cta__title {
+  margin-bottom: var(--spacing-tiny);
+}
+
+.dialogue-cta__image {
+  width: 200px;
+  height: 200px;
+  align-self: center;
+  overflow: hidden;
+  border-radius: 50%;
+}
+
+.dialogue-cta__image > * {
+  object-fit: cover;
+  object-position: top;
+}
+
+.dialogue-cta__body {
+  margin-bottom: var(--spacing-small);
+  max-width: 55ch;
+}
+
+.dialogue-cta__body--quote {
+  font-style: italic;
+  quotes: auto;
+}
+
+.dialogue-cta__body--quote > p {
+  display: inline;
+}
+
+.dialogue-cta__body > p > a {
+  background: transparent linear-gradient(to top, transparent 1px, var(--html-blue) 1px, var(--html-blue) 2px, transparent 2px);
+  color: var(--html-blue);
+  padding-bottom: .15rem;
+}
+
+.dialogue-cta__body > p > a:hover {
+  opacity: 0.8;
+}
+
+.dialogue-cta__body--quote::before {
+  content: open-quote;
+}
+
+.dialogue-cta__body--quote::after {
+  content: close-quote;
+}
+
+.dialogue-cta__ctas {
+  display: inline-flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: var(--spacing-small);
+}
+
+@media (min-width: 600px) {
+  .dialogue-cta {
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .dialogue-cta__image {
+    flex-shrink: 0;
+  }
+}
+
+@media (min-width: 1000px) {
+  .dialogue-cta {
+    grid-column-start: 8;
+    grid-column-end: 47;
+  }
+}
+</style>
