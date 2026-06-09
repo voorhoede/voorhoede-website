c8fe4203f8157cbda13b807cf0883311f0159c2b
Selwyn <talk@selwyn.cc>
2026-06-09 14:04:56 +0200

    WIP: Use fragments-and-blocks environment

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,3 +1,3 @@
-export const datocmsEnvironment = 'fix-minor-ui';
+export const datocmsEnvironment = 'fragments-and-blocks';
 export const mastodonUrl = 'https://fosstodon.org/@devoorhoede';
 export const datocmsContentApiUrl = 'https://graphql.datocms.com/';
