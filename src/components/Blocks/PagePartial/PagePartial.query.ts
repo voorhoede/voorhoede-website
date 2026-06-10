ce7a89499ff4affe7c498a77562a1486c345faf6
milailijevic <mila.ilijevic@gmail.com>
2026-06-10 11:29:40 +0200

    feat: support all migrated section types in page partial

--- src/components/Blocks/PagePartial/PagePartial.query.ts
+++ src/components/Blocks/PagePartial/PagePartial.query.ts
@@ -1,7 +1,10 @@
 import { graphql } from "~/utils/graphql";
 import { CaseListFragment } from "../CaseList/CaseList.query";
 import { DialogueCtaFragment } from "../DialogueCta/DialogueCta.query";
+import { ImageCardGridFragment } from "../ImageCardGrid/ImageCardGrid.query";
 import { LogoGridFragment } from "../LogoGrid/LogoGrid.query";
+import { PageHeaderFragment } from "../PageHeader/PageHeader.query";
+import { TextImageFragment } from "../TextImage/TextImage.query";
 
 export const PagePartialFragment = graphql(
   `fragment PagePartialFragment on PagePartialBlockRecord {
@@ -14,9 +17,19 @@ export const PagePartialFragment = graphql(
           __typename
           ...CaseListFragment
           ...DialogueCtaFragment
+          ...ImageCardGridFragment
           ...LogoGridFragment
+          ...PageHeaderFragment
+          ...TextImageFragment
         }
       }
     }`,
-  [CaseListFragment, DialogueCtaFragment, LogoGridFragment],
+  [
+    CaseListFragment,
+    DialogueCtaFragment,
+    ImageCardGridFragment,
+    LogoGridFragment,
+    PageHeaderFragment,
+    TextImageFragment,
+  ],
 );
