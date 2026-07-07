bcf6e5fbd4ec08184b9ad68f62842866ee491a2b
MarleenEliza <marleen.dijkman@outlook.com>
2026-07-07 17:16:32 +0200

    add sturcutred text styles plugin for textBlock styling

---
+++ migrations/1783437273_addDatoPlugin.ts
@@ -0,0 +1,48 @@
+import type { Client} from "datocms/lib/cma-client-node";
+
+export default async function (client: Client) {
+  console.log("Manage upload filters");
+
+  console.log('Install plugin "Custom Text Styles"');
+  await client.plugins.create({
+    id: "G_baOmXrQzahJBvJO_qwcw",
+    package_name: "datocms-plugin-custom-text-styles",
+  });
+  await client.plugins.update("G_baOmXrQzahJBvJO_qwcw", {
+    parameters: {
+      customMarks: [
+        {
+          css: "color: blue;",
+          icon: { label: "Paintbrush", value: "paintbrush" },
+          slug: "blue",
+          title: "Blue",
+          isOpen: true,
+          keyboardShortcut: "ctrl+shift+p",
+        },
+      ],
+      customStyles: [
+        {
+          css: "text-align: center;",
+          slug: "centered",
+          nodes: [
+            { label: "Paragraph", value: "paragraph" },
+            { label: "Heading", value: "heading" },
+          ],
+          title: "Centered",
+          isOpen: false,
+        },
+        {
+          css: "font-size: 30px;\nline-height: 1.5;",
+          slug: "testimonial",
+          nodes: [
+            { label: "Paragraph", value: "paragraph" },
+            { label: "Heading", value: "heading" },
+          ],
+          title: "Testimonial",
+          isOpen: true,
+        },
+      ],
+    },
+  });
+
+}
