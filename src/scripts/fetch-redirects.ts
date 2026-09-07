d8ee035e34d57cfa0a9a3e956492943110cacade
Selwyn <talk@selwyn.cc>
2026-09-07 12:16:35 +0200

    Shrink PR scope: Revert redirect pagination

--- src/scripts/fetch-redirects.ts
+++ src/scripts/fetch-redirects.ts
@@ -1,27 +1,16 @@
 import { datocmsFetch } from '../lib/datocms-fetch.js';
 
-const pageSize = 100;
-
-export const fetchRedirects = async () => {
-  const redirectRules = [];
-
-  while (true) {
-    const { data } = await datocmsFetch({
-      query: `
-        query Redirects($first: IntType!, $skip: IntType!) {
-          allRedirectRules(first: $first, skip: $skip) {
-            from
-            to
-            statusCode
-          }
+export const fetchRedirects = () => (
+  datocmsFetch({
+    query: `
+      query Redirects {
+        allRedirectRules(first: 100) {
+          from
+          to
+          statusCode
         }
-      `,
-      variables: { first: pageSize, skip: redirectRules.length },
-    });
-    redirectRules.push(...data.allRedirectRules);
-
-    if (data.allRedirectRules.length < pageSize) {
-      return redirectRules;
-    }
-  }
-};
+      }
+    `,
+  })
+    .then(({ data }) => data.allRedirectRules)
+);
